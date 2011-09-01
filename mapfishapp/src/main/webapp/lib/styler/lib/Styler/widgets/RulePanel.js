a12f9e23e928b982cbe0e489ce2eaab9e8e4628b
bbinet <bruno.binet@camptocamp.com>
2011-09-01 14:07:31 +0000

    fix limit by scale layout in advanced tab: in ext3 collapsed fieldset are not rendered so we have to call doLayout by hand when expanding the fieldset

--- mapfishapp/src/main/webapp/lib/styler/lib/Styler/widgets/RulePanel.js
+++ mapfishapp/src/main/webapp/lib/styler/lib/Styler/widgets/RulePanel.js
@@ -247,6 +247,10 @@ Styler.RulePanel = Ext.extend(Ext.TabPanel, {
                             this.fireEvent("change", this, this.rule)
                         },
                         expand: function(){
+                            // in ext3 collapsed fieldset are not rendered
+                            // so we have to call doLayout by hand when
+                            // expanding the fieldset
+                            this.scaleLimitPanel.doLayout();
                             if (this.scaleLimitPanel.scaleSlider) {
                                 this.scaleLimitPanel.scaleSlider.syncThumbs();
                             }
