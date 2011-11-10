13ba759d06a9bed214bc8409eaaef0daabd0e6ac
jesseeichar <jesse.eichar@camptocamp.com>
2011-11-10 14:53:15 +0000

    removed accidental files

--- config/pigma_production/DeployScript.groovy
+++
@@ -1,8 +0,0 @@
-def ssh = new SSH(log:log,settings:settings,host:"ns383241.ovh.net")
-def deploy = new C2CDeploy(project,ssh)
-deploy.apacheConfDir = '/var/www/aquitaine_pigma/conf'
-deploy.artifacts = new Artifacts(deploy.project, { artifact -> 
-	if(artifact.name.startsWith("geoserver-webapp")) return "geoserver.war"
-	else Artifacts.standardGeorchestraAliasFunction(artifact)
-})
-deploy.deploy()
--- config/pigma_production/cas-server-webapp/WEB-INF/view/jsp/default/ui/includes/bottom.jsp
+++
@@ -1,9 +0,0 @@
-		</div>
-	    <div id="footer">
-	        <div>
-	            <p>Copyright &copy; 2005-2007 JA-SIG. All rights reserved.</p>
-	            <p>Powered by <a href="http://www.ja-sig.org/products/cas/">JA-SIG Central Authentication Service <%=org.jasig.cas.CasVersion.getVersion()%></a></p>
-	        </div>
-	    </div>
-	</body>
-</html>
