bbd5139417b59dbf7ef89bf344fe6a7b1a65f4c5
fvanderbiest <francois.vanderbiest@camptocamp.com>
2011-12-06 21:04:20 +0000

    mapfishapp: removed a FIXME

--- mapfishapp/src/main/webapp/app/js/GEOR_wmc.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_wmc.js
@@ -13,7 +13,9 @@
  */
 
 /*
- * FIXME: whether to include either v1_0_0.js or v1_1_0.js or both?
+ * Should we include either v1_0_0.js or v1_1_0.js or both?
+ * This app produces v1.1 compliant WMCs, but one might want 
+ * to restore v1.0 compliant WMCs too => both formats are useful
  * @include OpenLayers/Format/WMC/v1_0_0.js
  * @include OpenLayers/Format/WMC/v1_1_0.js
  * @include GeoExt/data/WMCReader.js
