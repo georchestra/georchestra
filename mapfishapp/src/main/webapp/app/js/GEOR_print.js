18d237b4bfa01c5b3dfa5825ea8c261617ab2093
jesseeichar <jesse.eichar@camptocamp.com>
2011-12-13 14:00:03 +0000

    added missing ,

--- mapfishapp/src/main/webapp/app/js/GEOR_print.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_print.js
@@ -153,7 +153,7 @@ GEOR.print = (function() {
                     // set a custom PDF file name:
                     pp.customParams.outputFilename = [
                         GEOR.config.PRINT_FILENAME_PREFIX,
-                        "${yyyy-MM-dd}"
+                        "${yyyy-MM-dd}",
                         Math.random().toString(16).slice(2,10)
                     ].join('_');
                 },
