d381f991fd38c3b59fa6cd6734ed64e95f1af3cf
fvanderbiest <francois.vanderbiest@camptocamp.com>
2012-04-02 08:22:25 +0000

    analytics: fixed incorrect link to metadata.show

--- analytics/src/main/webapp/site/js/app/view/GeonetworkFiles.js
+++ analytics/src/main/webapp/site/js/app/view/GeonetworkFiles.js
@@ -9,7 +9,7 @@ Ext.define('Analytics.view.GeonetworkFiles', {
             text: 'Métadonnée',
             renderer: function(v) {
                 if (!v) return;
-                var url = 'http://ids.pigma.org/geonetwork/srv/fr/metadata.show?uuid='+v; // TODO: config for base URL
+                var url = 'http://ids.pigma.org/geonetwork/srv/fr/metadata.show?id='+v; // TODO: config for base URL
                 return '<a href="'+url+'" target="_blank">'+v+'</a>'
             }
         }, {
