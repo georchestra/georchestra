af5dd614fb4d85e6cb9ba2d186e629bb78db8d63
fvanderbiest <francois.vanderbiest@camptocamp.com>
2012-04-10 07:37:58 +0000

    analytics: optimized resources loading

--- analytics/src/main/webapp/site/js/app/Application.js
+++ analytics/src/main/webapp/site/js/app/Application.js
@@ -1,8 +1,9 @@
 Ext.Loader.setConfig({
     enabled: true,
-    disableCaching: true, // TODO: set to false for PROD
+    disableCaching: false,
     paths: {
-        'Ext': 'resources/site/js/lib/external/ext/src'
+        'Ext': 'resources/site/js/lib/external/ext/src',
+        'Analytics': 'resources/site/js/app'
     }
 });
 
@@ -10,12 +11,19 @@ Ext.require([
     'Ext.grid.Panel',
     'Ext.data.Store',
     'Ext.layout.container.Border',
-    'Ext.tab.Panel'
+    'Ext.tab.Panel',
+    // 'Analytics.store.*' surprisingly does not work, so:
+    'Analytics.store.OGCLayers',
+    'Analytics.store.OGCUsers',
+    'Analytics.store.GeonetworkFiles',
+    'Analytics.store.GeonetworkUsers',
+    'Analytics.store.ExtractorLayers',
+    'Analytics.store.ExtractorUsers'
 ]);
 
 Ext.application({
     name: 'Analytics',
-    appFolder:'resources/site/js/app', // strange that it is not a config option for Ext.Loader path
+    appFolder:'resources/site/js/app', // strange that it needs to be here // kind of redundant with the above Ext.Loader paths
     autoCreateViewport: true, // By setting autoCreateViewport to true, the framework will, by convention, include the app/view/Viewport.js file
     models: ['OGCLayer','OGCUser','GeonetworkFile','GeonetworkUser','ExtractorLayer','ExtractorUser'],
     stores: ['OGCLayers','OGCUsers','GeonetworkFiles','GeonetworkUsers','ExtractorLayers','ExtractorUsers'],
