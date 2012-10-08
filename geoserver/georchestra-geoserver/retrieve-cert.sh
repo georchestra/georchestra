3b227b1d8704cca10b87c80b401923cb7e8d9638
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-10-08 14:12:07 +0200

    change to geoserver build to simplify build and hopefully make it more robust

--- geoserver/georchestra-geoserver/retrieve-cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# usage: retrieve-cert.sh remote.host.name [port]
-#
-REMHOST=$1
-REMPORT=${2:-443}
-echo |\
-openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |\
-sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
