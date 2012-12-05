6b14b39269318b33abdecac1dd6d2fc4eaef0749
Sylvain Lesage <severo@rednegra.net>
2012-12-05 15:12:30 -0400

    Internationalization of catalogapp - template is updated

---
+++ catalogapp/src/main/webapp/app/js/GEOR_Lang/es.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) Camptocamp
+ *
+ * This file is part of geOrchestra
+ *
+ * geOrchestra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with geOrchestra.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Spanish translation file
+ */
+OpenLayers.Lang.es = OpenLayers.Util.extend(OpenLayers.Lang.es, {
+    /* General purpose strings */
+    "Yes": "Si",
+    "No": "No",
+    "OK": "OK",
+    "Cancel": "Cancelar",
+    /* GEOR.js */
+    "Which data are you searching for ?": "¿Que datos esta buscando?",
+    "On which area ?": "¿Sobre que territorio?",
+    "When ?": "¿Cuando?",
+    'clean': 'borrar',
+    'search': 'buscar',
+    "various.results": "${RESULTS} fichas seleccionadas",
+    "one.result": "1 ficha seleccionada",
+    /* GEOR_dataview.js strings */
+    'View WMS layer': "<b>Visualizar</b> la capa WMS <b>${NAME}</b>",
+    'Download WMS layer': "<b>Descargar</b> la capa WMS <b>${NAME}</b>",
+    'View WMS service': "<b>Visualizar</b> el servicio WMS <b>${NAME}</b>",
+    'Download WMS service': "<b>Descargar</b> el servicio WMS <b>${NAME}</b>",
+    'Download data': "Descargar el dato ${NAME}",
+    'zoom': 'extensión espacial',
+    'record': 'ficha',
+    'Keywords: ': 'Palabras clave: ',
+    "Server NAME": "Servidor ${NAME}",
+    "Oops, a problem occured.": "Oops, occurrió un problema.",
+    /* GEOR_nav.js strings */
+    "go to first results": "ir a los primeros resultados",
+    "previous page": "página anterior",
+    "next page": "página siguiente",
+    "go to last results": "ir a los últimos resultados",
+    "No result": "Ningún resultado",
+    "Results N1 to N2 of N": "Resultados ${N1} a ${N2} de ${N}",
+    "Result N1 to N2 of N": "Resultado ${N1} a ${N2} de ${N}",
+    /* GEOR_what.js strings */
+    'enter one or more keywords': 'ingresar una o varias palabras clave'
+    // no trailing comma
+});
