40dc8b9a536faea128e270a96b08e9624e5fb7cb
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-01-14 14:00:42 +0100

    mapfishapp: icon for add layer button

--- mapfishapp/src/main/webapp/app/js/GEOR_managelayers.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_managelayers.js
@@ -823,6 +823,7 @@ GEOR.managelayers = (function() {
                 root: layerContainer,
                 buttons: [{
                     text: tr("Add layers"),
+                    iconCls: 'layers-add',
                     handler: function() {
                         if (!layerFinder) {
                             layerFinder = GEOR.layerfinder.create(layerStore, this.el);
