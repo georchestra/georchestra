2a253c94b13d8c16ba735cfb603cf632f644f337
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-01-29 10:47:59 +0100

    basic UI with a file upload form

---
+++ mapfishapp/src/main/webapp/app/js/GEOR_fileupload.js
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) Camptocamp
+ *
+ * This file is part of geOrchestra
+ *
+ * geOrchestra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with geOrchestra.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * @include GEOR_util.js
+ * @include GEOR_config.js
+ */
+
+Ext.namespace("GEOR");
+
+GEOR.fileupload = (function() {
+
+    /*
+     * Private
+     */
+
+    var observable = new Ext.util.Observable();
+    observable.addEvents(
+        /**
+         * Event: selectionchanged
+         * Fires when the selection has changed
+         *
+         * Listener arguments:
+         * records - {Array} array of selected records
+         */
+        "selectionchanged"
+    );
+
+    /**
+     * Property: tr
+     * {Function} an alias to OpenLayers.i18n
+     */
+    var tr = null;
+
+    /*
+     * Public
+     */
+    return {
+        /*
+         * Observable object
+         */
+        events: observable,
+
+        /**
+         * APIMethod: getPanel
+         * Return the panel for the WMS browser
+         *
+         * Parameters:
+         * options - {Object} Hash with key: srs (the map srs).
+         * The other options will be applied to panel
+         *
+         * Returns:
+         * {Ext.Panel}
+         */
+        getPanel: function(options) {
+            tr = OpenLayers.i18n;
+            var srs = options.srs;
+            delete options.srs;
+
+            return new Ext.Panel(Ext.apply({
+                title: tr("Local file"),
+                layout: 'vbox',
+                layoutConfig: {
+                    align: 'stretch'
+                },
+                defaults: {border: false},
+                items: [{
+                    xtype: 'form',
+                    region: "north",
+                    fileUpload: true,
+                    bodyStyle: 'padding:10px',
+                    labelWidth: 100,
+                    height: 80,
+                    monitorValid: true,
+                    buttonAlign: 'right',
+                    items: [{
+                        xtype: 'textfield',
+                        inputType: 'file',
+                        name: 'geofile',
+                        labelSeparator: tr("labelSeparator"),
+                        fieldLabel: tr("File"),
+                        allowBlank: false,
+                        blankText: tr("The file is required.")
+                    }],
+                    buttons: [{
+                        text: tr("upload"),
+                        handler: function(btn) {
+                            form = btn.ownerCt.ownerCt.getForm();
+                            if (form.isValid()) {
+                                form.submit({
+                                    url: "ws/togeojson/",
+                                    // Beware: form submission requires a *success* parameter in json response
+                                    // As said in http://extjs.com/learn/Manual:RESTful_Web_Services
+                                    // "Ext.form.BasicForm hopefully becomes HTTP Status Code aware!"
+                                    success: function(form, action) {
+                                        var fc = (new OpenLayers.Format.GeoJSON()).read(action.response.responseText);
+                                        alert(fc.features.length + " features in file");
+                                    },
+                                    failure: function(form, action){
+                                        alert("Error : " + action.result.msg);
+                                    },
+                                    scope: this
+                                });
+                            }
+                            
+                        }
+                    }]
+                }, {
+                    html: 'feature grid in here',
+                    flex: 1
+                }]
+            }, options));
+        },
+
+        /**
+         * APIMethod: clearSelection
+         * Clears the current selection
+         */
+        clearSelection: function() {
+            //cbxSm.clearSelections();
+        }
+    };
+})();
