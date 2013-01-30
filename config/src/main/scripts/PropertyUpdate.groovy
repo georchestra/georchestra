5f4ee781650f41d6f59e9138984d8207ff9b6f5f
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-01-30 14:10:46 +0100

    added new updater options

--- config/src/main/scripts/PropertyUpdate.groovy
+++ config/src/main/scripts/PropertyUpdate.groovy
@@ -1,35 +1,22 @@
 /**
  * Helper class for updating property files.  Example usage:
  * 
- * 	  new PropertyUpdate(
-		  from: 'defaults'+SEP+'security-proxy'+SEP+'maven.filter', 
-		  to: 'security-proxy'+SEP+'maven.filter').update { properties ->
-        println("pp\n\n"+properties)
+ * new PropertyUpdate(
+      path: 'maven.filter',
+		  from: 'defaults/security-proxy', 
+		  to: 'security-proxy').update { properties ->
 		    properties['shared.server.name'] = host
 		    properties['shared.default.log.level'] = logLevel
 		    properties['application_home'] = applicationHome
 		    properties['shared.ldapUrl'] = ldapUrl
 	  }
-	  
- * The 'from' parameter is a string and is relative to the root of the config project directory
- * The 'to' parameter is also a string but is relative to the directory that contains generated files.  These files override the
- * files in the normal configuration
- * The update method is called with a closure that will update the properties read from the 'from' file and writes the updated properties
- * to the 'to' file.  
  * 
- * Note: if the 'from' parameter is not defined then the properties will be empty when passed to the update method.
- * Note: if the 'from' parameter is defined then the file must exist or an error will be thrown
  */
-class PropertyUpdate {
-	String from, to
+class PropertyUpdate extends AbstractUpdater {
 	def update(closure) {
-	  def params = Parameters.get
 		def properties = new Properties()
-		if(from != null) {
-		  def fromFile = new File(params.basedirFile, from)
-		  if (!fromFile.exists()) {
-		    throw new IllegalStateException("$fromFile does not exist.")
-		  }
+		def fromFile = getFromFile()
+		if(fromFile != null) {
 		  params.log.info("Loading parameters to update from $fromFile")
 		  fromFile.withReader { r -> 
 			  properties.load(r)
@@ -38,8 +25,7 @@ class PropertyUpdate {
 		
 		closure(properties)
 
-		def toFile = new File(params.outputDir,to)
-		toFile.parentFile.mkdirs()
+		def toFile = getToFile()
 		params.log.info("Writing updated parameters to $toFile")
 		params.log.debug("writing "+properties+" to "+toFile)
 
