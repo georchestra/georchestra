16df5082ad2e67b92715d2919a2275df243acc12
Mauricio Pazos <mauricio.pazos@gmail.com>
2013-02-05 12:41:46 +0100

    issue4291 - including OGRDataStore to retrieve feature from shp, mif, tab, ... files

---
+++ mapfishapp/src/main/java/mapfishapp/ws/upload/JSONUtil.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package mapfishapp.ws.upload;
+
+import org.json.JSONObject;
+import org.opengis.feature.simple.SimpleFeature;
+
+/**
+ * @author Mauricio Pazos
+ *
+ */
+final class JSONUtil {
+	
+	private JSONUtil(){
+		// utility class
+	}
+
+	public static JSONObject asJSONObject(SimpleFeature feature) {
+		// TODO Auto-generated method stub
+		return new JSONObject();
+	}
+
+}
