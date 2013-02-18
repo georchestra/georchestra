abba2e8494434e16c9bd2dd32f49292613264e4e
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-02-18 10:58:51 +0100

    added updated config from geonetwork branch

--- config/src/main/scripts/PropertyUpdate.groovy
+++ config/src/main/scripts/PropertyUpdate.groovy
@@ -1,16 +1,32 @@
-class PropertyUpdate {
-	String from, to
+/**
+ * Helper class for updating property files.  Example usage:
+ * 
+ * new PropertyUpdate(
+      path: 'maven.filter',
+		  from: 'defaults/security-proxy', 
+		  to: 'security-proxy').update { properties ->
+		    properties['shared.server.name'] = host
+		    properties['shared.default.log.level'] = logLevel
+		    properties['application_home'] = applicationHome
+		    properties['shared.ldapUrl'] = ldapUrl
+	  }
+ * 
+ */
+class PropertyUpdate extends AbstractUpdater {
 	def update(closure) {
-	  def params = Parameters.get
 		def properties = new Properties()
-		new File(params.basedirFile, from).withReader { r -> 
-			properties.load(r)
-		}
+		def fromFile = getFromFile()
+		if(fromFile != null) {
+		  params.log.info("Loading parameters to update from $fromFile")
+		  fromFile.withReader { r -> 
+			  properties.load(r)
+		  }
+	  }
 		
 		closure(properties)
 
-		def toFile = new File(params.outputDir,to)
-		toFile.parentFile.mkdirs()
+		def toFile = getToFile()
+		params.log.info("Writing updated parameters to $toFile")
 		params.log.debug("writing "+properties+" to "+toFile)
 
 	  toFile.withWriter('UTF-8'){ w -> properties.store(w,"updated by pigma's GenerateConfig class")}
---
+++ config/src/main/scripts/TextUpdate.groovy
@@ -0,0 +1,37 @@
+/**
+ *
+ * Updates a text file by replacing all sections of the file that match the patterns
+ * and outputs it to the configuration
+ * Example Usage:
+ *
+ *  new TextUpdate(
+	    path:  'apps/georchestra/js/Settings.js',
+	    fromProject: "geonetwork",
+	    from: 'web-client/src/main/resources/',
+	    to: 'geonetwork-client/',
+	    patternsToReplace: [ /GeoNetwork\.Util\.defaultLocale\s*=\s*'eng'/: "GeoNetwork.Util.defaultLocale = 'fre'"]
+	    ).update()
+	*
+	* for a description of path, fromProject, from and to see AbstractUpdater
+	* 
+	* Note: unlike many other updaters a from file is required
+  */
+class TextUpdate extends AbstractUpdater {
+  def patternsToReplace = [:]
+  def update() {
+    def fromFile = getFromFile()
+    def toFile = getToFile()
+    assert fromFile.exists(), "$fromFile does not exist"
+
+    params.log.info ("updating text file.\n\tfrom: $fromFile\n\tto:$toFile")
+
+    def text = fromFile.text
+    patternsToReplace.each {entry ->
+      assert text =~ entry.key, "Unable to find any matches for ${entry.key}"
+      params.log.info("Performing following replacement: $entry")
+      text = text.replaceAll(entry.key, entry.value)
+    }
+
+    toFile << text.getBytes("UTF-8")
+  }
+}
