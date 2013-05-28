georchestra/georchestra

commit cf1a33215ff4b405338f4fffd6eb4d3c35c3ac95

mapfishapp: temporary fix for layers not showing up after WMC is restored

mapfishapp/src/main/webapp/app/js/GEOR_wmc.js
@@ -25,6 +25,15 @@
 
 Ext.namespace("GEOR");
 
+// temporary hack, waiting that GeoServer publishes correct min/maxScale in capabilities
+OpenLayers.Format.WMC.v1_1_0.prototype.read_sld_MinScaleDenominator = function(layerContext, node) {
+    layerContext.maxScale = null;
+};
+OpenLayers.Format.WMC.v1_1_0.prototype.read_sld_MaxScaleDenominator = function(layerContext, node) {
+    layerContext.minScale = null;
+};
+// end hack
+
 GEOR.wmc = (function() {
     /*
      * Private

commit 46bdc2fc54d988e3df2f23560416b2ba00c882d7

mapfishapp: fixed regression introduced by cf1a3321

mapfishapp/src/main/webapp/app/js/GEOR_wmc.js
@@ -26,6 +26,9 @@
 Ext.namespace("GEOR");
 
 // temporary hack, waiting that GeoServer publishes correct min/maxScale in capabilities
+/*
+ * @requires OpenLayers/Format/WMC/v1_1_0.js
+ */
 OpenLayers.Format.WMC.v1_1_0.prototype.read_sld_MinScaleDenominator = function(layerContext, node) {
     layerContext.maxScale = null;
 };
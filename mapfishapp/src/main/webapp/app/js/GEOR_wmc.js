46bdc2fc54d988e3df2f23560416b2ba00c882d7
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-05-28 11:34:23 +0200

    mapfishapp: fixed regression introduced by cf1a3321

--- mapfishapp/src/main/webapp/app/js/GEOR_wmc.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_wmc.js
@@ -26,6 +26,9 @@
 Ext.namespace("GEOR");
 
 // temporary hack, waiting that GeoServer publishes correct min/maxScale in capabilities
+/*
+ * @requires OpenLayers/Format/WMC/v1_1_0.js
+ */
 OpenLayers.Format.WMC.v1_1_0.prototype.read_sld_MinScaleDenominator = function(layerContext, node) {
     layerContext.maxScale = null;
 };
