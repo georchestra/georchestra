1c02dc388d1ee21390d523d500ebf5aacdca1573
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-07-05 12:24:17 +0200

    End 2 end tests

--- ldapadmin/test/e2e/scenarios.js
+++ ldapadmin/test/e2e/scenarios.js
@@ -9,37 +9,40 @@ describe('my app', function() {
   });
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should automatically redirect to /users when location hash/fragment is empty', function() {
+    expect(browser().location().url()).toBe("/users");
   });
 
 
-  describe('view1', function() {
+  describe('users', function() {
 
     beforeEach(function() {
-      browser().navigateTo('#/view1');
+      browser().navigateTo('#/users');
     });
 
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
+    it('should render a list of 100 users', function() {
+      expect(repeater('.users tr').count()).toEqual(100);
     });
-
   });
 
+  describe('group', function() {
 
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
+    it('should render a list of 5 users', function() {
+      browser().navigateTo('#/groups/Administrator');
+      expect(repeater('.users tr').count()).toEqual(5);
     });
 
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
+    it('should select a group', function() {
+      element('.groups div:nth-child(2) a').click();
+      expect(browser().location().url()).toBe('/groups/SV_XXX');
     });
+  });
 
+  describe('create user', function() {
+    it('should show the show the edit view', function() {
+      element('#new_user').click();
+      expect(browser().location().url()).toBe('/users/new');
+      expect(input('user.name').val()).toBe('');
+    });
   });
 });
