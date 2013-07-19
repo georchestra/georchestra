cdc58435a1c7daadf1ba7a536861c2693b55292a
Sylvain Lesage <severo@rednegra.net>
2013-07-19 09:33:46 +0200

    mapfishapp - missing var's (thx @fvanderbiest)

--- mapfishapp/src/main/webapp/app/js/GEOR_print.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_print.js
@@ -127,12 +127,11 @@ GEOR.print = (function() {
      * {String} The projection string
      */
     var getProjection = function() {
-        var epsg = layerStore.map.getProjection();
+        var c = '', s = '', epsg = layerStore.map.getProjection();
         proj = Proj4js.defs[epsg];
         if (!proj) {
             return '';
         }
-        s = '';
         Ext.each(proj.split('+'), function(r){
             c = r.split("title=");
             if (c.length > 1) {
