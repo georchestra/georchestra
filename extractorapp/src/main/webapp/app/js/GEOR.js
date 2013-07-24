909393b97a078aacd411e2562da29da257d86caf
Sylvain Lesage <severo@rednegra.net>
2013-07-24 20:36:47 +0200

    extractorapp - move the extraction button in a new bottom panel

--- extractorapp/src/main/webapp/app/js/GEOR.js
+++ extractorapp/src/main/webapp/app/js/GEOR.js
@@ -212,15 +212,7 @@ Ext.namespace("GEOR");
                 items: [{
                     layout: "fit",
                     title: tr("Configuration"),
-                    items: GEOR.layerstree.create(),
-                    bbar: [ '->',
-                        {
-                            id: "geor-btn-extract-id",
-                            text: tr("Extract the selected layers"),
-                            iconCls: "geor-btn-extract",
-                            handler: extractBtnHandler
-                        }
-                    ]
+                    items: GEOR.layerstree.create()
                 }, {
                     layout:"border",
                     title: tr("Recenter"),
@@ -240,6 +232,16 @@ Ext.namespace("GEOR");
                             region: 'center'
                         }
                     ]
+                }]
+            }, {
+                layout: "form",
+                region: 'south',
+                buttons: [{
+                    xtype: 'button',
+                    id: "geor-btn-extract-id",
+                    text: tr("Extract the selected layers"),
+                    iconCls: "geor-btn-extract",
+                    handler: extractBtnHandler
                 }]
             }]
         });
