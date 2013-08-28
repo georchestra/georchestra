ac8369946c924d076f79b4e480966913340a05bf
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-08-28 11:40:36 +0200

    mapfishapp: srs combo width

--- mapfishapp/src/main/webapp/app/js/GEOR_mappanel.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_mappanel.js
@@ -127,7 +127,7 @@ GEOR.mappanel = (function() {
         items.push(tr("Coordonnées en "));
         items.push({
             xtype: 'combo',
-            width: 100,
+            width: 110,
             store: srsList,
             value: srsList[0][1],
             editable: false,
