d9733d24c595b9d528d955066310d5b022774a51
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-09-11 13:40:21 +0200

    fixed mapfishapp build by removing OpenLayers/Layer/OSM.js

--- mapfishapp/src/main/webapp/app/js/GEOR_map.js
+++ mapfishapp/src/main/webapp/app/js/GEOR_map.js
@@ -24,7 +24,6 @@
  * @include OpenLayers/Kinetic.js
  * @include OpenLayers/Layer/WMS.js
  * @include OpenLayers/Layer/XYZ.js
- * @include OpenLayers/Layer/OSM.js
  * @include OpenLayers/Layer/Grid.js
  * @include OpenLayers/BaseTypes/Size.js
  * @include GeoExt/data/LayerRecord.js
