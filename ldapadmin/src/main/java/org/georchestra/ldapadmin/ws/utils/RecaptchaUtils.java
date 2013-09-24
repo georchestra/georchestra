f61982e3c67217a7c8e59e8502c6edde0c50e829
Sylvain Lesage <severo@rednegra.net>
2013-09-24 17:32:04 +0200

    Add logs for recaptcha validation function

--- ldapadmin/src/main/java/org/georchestra/ldapadmin/ws/utils/RecaptchaUtils.java
+++ ldapadmin/src/main/java/org/georchestra/ldapadmin/ws/utils/RecaptchaUtils.java
@@ -6,6 +6,9 @@ import net.tanesha.recaptcha.ReCaptchaResponse;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Utility class to manage recaptcha.
  * 
@@ -14,6 +17,7 @@ import org.springframework.validation.Errors;
  */
 public class RecaptchaUtils {
 	
+	private static final Log LOG = LogFactory.getLog(RecaptchaUtils.class.getName());
 	private String remoteAddr;
 	private ReCaptcha reCaptcha;
 
@@ -27,6 +31,7 @@ public class RecaptchaUtils {
 		final String trimmedCaptcha = userResponse.trim();
 
 		if(!StringUtils.hasLength(trimmedCaptcha)){
+			LOG.info("The user response to recaptcha is empty.");
 			errors.rejectValue("recaptcha_response_field", "recaptcha_response_field.error.required", "required");
 		} else {
 			
@@ -35,6 +40,7 @@ public class RecaptchaUtils {
 					captchaGenerated, 
 					userResponse);
 			if(!captchaResponse.isValid()){
+				LOG.info("The user response to recaptcha is not valid. The error message is '" + captchaResponse.getErrorMessage() + "'");
 				errors.rejectValue("recaptcha_response_field", "recaptcha_response_field.error.captchaNoMatch", "The texts didn't match");
 			}
 		}
