c48cbb02f0565283f7eb5c83943ca31b32b38f4e
François Van Der Biest <francois.vanderbiest@gmail.com>
2013-10-27 16:34:16 +0100

    Revert "missing header.js, backport from master 1ab2f8abe52ead5aa95a09f05a4866df5c2ac2dc"
    This reverts commit 3a1c13c5fb564c4212ddba1642627c349c2a3218.

--- static/src/main/webapp/js/header.js
+++
@@ -1,6 +0,0 @@
-var _headerOnLoad = function(iframe) {
-    var innerDoc = iframe.contentDocument || iframe.contentWindow.document,
-        base = innerDoc.createElement('base');
-    base.setAttribute('target', '_parent');
-    innerDoc.getElementsByTagName('head')[0].appendChild(base);
-};
