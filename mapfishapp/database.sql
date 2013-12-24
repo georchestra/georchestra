772618db55c3448ff7bd49f868c0ba0fc96c706a
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-12-24 17:22:29 +0100

    not null table fields

--- mapfishapp/database.sql
+++ mapfishapp/database.sql
@@ -5,10 +5,10 @@ create schema mapfishapp;
 create table mapfishapp.geodocs (
   id bigserial primary key, -- 1 to 9223372036854775807 (~ 1E19)
   username varchar(200), -- can be NULL (eg: anonymous user)
-  standard varchar(3), -- eg: CSV, KML, SLD, WMC
-  standard_version varchar(5), -- eg: 1.0.0 or 2.2
-  raw_file_content text, -- file content
-  file_hash varchar(32), -- md5sum
+  standard varchar(3) not null, -- eg: CSV, KML, SLD, WMC
+  standard_version varchar(5), -- eg: 1.0.0 or 2.2 if applicable
+  raw_file_content text not null, -- file content
+  file_hash varchar(32) unique not null, -- md5sum
   created_at timestamp without time zone default NOW(), -- creation date
   last_access timestamp without time zone, -- last access date
   access_count integer default 0 -- access count, defaults to 0
