a6e9378d869fe5504559bea7f3a862e0bab44704
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-05-05 14:06:38 +0200

    Change security-proxy so that sec-username and sec-roles always are removed.
    Added new header provider in security-proxy that checks the currently logged in user and if that user is one of the "special"/"trusted" users then it will map
    
    imp-username -> sec-username
    imp-roles -> sec-roles
    
    Allowing trusted users to impersonate another user.  This is required for the extractorapp to enforce the security on the end user.

--- security-proxy/src/main/java/org/georchestra/security/SecurityRequestHeaderFilter.java
+++ security-proxy/src/main/java/org/georchestra/security/SecurityRequestHeaderFilter.java
@@ -7,10 +7,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.methods.HttpRequestBase;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -20,43 +16,13 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class SecurityRequestHeaderFilter implements HeaderFilter {
 	protected static transient Log log = LogFactory.getLog(SecurityRequestHeaderFilter.class);
-	
-    List<String> trustedHosts = Collections.emptyList();
+
 
     @Override
     public boolean filter(String headerName, HttpServletRequest originalRequest, HttpRequestBase proxyRequest) {
-    	try {
-	        String remoteHost = originalRequest.getRemoteHost();
-	        InetAddress remoteHostAddress =  InetAddress.getByName(remoteHost);
-	        
-	        for (String host : trustedHosts) {
-	
-	        	InetAddress hostAddress = InetAddress.getByName(host);
-	        	
-	        	if (log.isDebugEnabled()) {
-	            	log.debug("filter: headerName: " + headerName + " check remote host: " + remoteHost + " against: " + host);
-	            }
-	        	
-	        	if (remoteHostAddress.equals(hostAddress)) {
-	        		if (log.isDebugEnabled()) {
-	        			log.debug("Return false for header: " + headerName + " because host: " + remoteHost + " is trusted.");
-	        		}
-	                return false;
-	            }
-	        }
-	        //
-	        // Return false if host is not trusted and header is sensitive
-	        //
-	        return headerName.equalsIgnoreCase("sec-username") ||
-	                headerName.equalsIgnoreCase("sec-roles");
-
-    	} catch (UnknownHostException e) {
-			log.error(e);
-			throw new IllegalStateException(e.getMessage() +  ". Check the trusted host configuration.");
-		}
-    }
-
-    public void setTrustedHosts(List<String> trustedHosts) {
-        this.trustedHosts = trustedHosts;
+	        return headerName.equalsIgnoreCase(HeaderNames.SEC_USERNAME) ||
+	                headerName.equalsIgnoreCase(HeaderNames.SEC_ROLES) ||
+	                headerName.equalsIgnoreCase(HeaderNames.IMP_USERNAME) ||
+	                headerName.equalsIgnoreCase(HeaderNames.IMP_ROLES);
     }
 }
