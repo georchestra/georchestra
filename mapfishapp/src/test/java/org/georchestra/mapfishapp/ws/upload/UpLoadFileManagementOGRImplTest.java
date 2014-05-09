9086fba4a2d69978f86441b3dffb2da06a0a32df
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-05-09 21:40:42 +0200

    Hardenning OGR testsuite
    Ensures that conditions are fullfilled before executing tests (OGR
    bindings as well as the native lib should be available before attempting
    to run the tests).

--- mapfishapp/src/test/java/org/georchestra/mapfishapp/ws/upload/UpLoadFileManagementOGRImplTest.java
+++ mapfishapp/src/test/java/org/georchestra/mapfishapp/ws/upload/UpLoadFileManagementOGRImplTest.java
@@ -5,8 +5,10 @@ package org.georchestra.mapfishapp.ws.upload;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assume.assumeNoException;
 /**
  * Test case for {@link UpLoadFileManagement} set to use the OGR implementation  
  * 
@@ -28,10 +30,20 @@ public class UpLoadFileManagementOGRImplTest extends UpLoadFileManagementGTImplT
 	protected UpLoadFileManagement create() throws IOException{
 		return UpLoadFileManagement.create(UpLoadFileManagement.Implementation.ogr);
 	}
-	
+	@Before
+	public void setUp() {
+		try {
+			// First, the bindings should be available in the classpath
+			Class.forName("org.gdal.ogr.ogr");
+			// Second, the JVM should be able to load the native lib.
+			System.loadLibrary("ogrjni");
+		} catch (Throwable e) {
+			assumeNoException(e);
+		}
+	}
 	@Test
 	public void testGPXAsJSON() throws Exception {
-		
+
 		String fileName = "wp.gpx";
 		String fullName = makeFullName(fileName);
 		
