b3b9722e7b687e48ede158f20d43c77ba9b99ae4
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-06-14 22:32:23 +0200

    ldapadmin - improving testsuite (coverage -> 15%)
    - finishing tests on PasswordRecoveryFormController
    - Adding test for NewPasswordFormController

--- ldapadmin/src/test/java/org/georchestra/ldapadmin/ws/passwordrecovery/PasswordRecoveryFormControllerTest.java
+++ ldapadmin/src/test/java/org/georchestra/ldapadmin/ws/passwordrecovery/PasswordRecoveryFormControllerTest.java
@@ -9,11 +9,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import net.tanesha.recaptcha.ReCaptcha;
+import net.tanesha.recaptcha.ReCaptchaResponse;
 
 import org.georchestra.ldapadmin.Configuration;
 import org.georchestra.ldapadmin.bs.ReCaptchaParameters;
 import org.georchestra.ldapadmin.ds.AccountDao;
+import org.georchestra.ldapadmin.ds.DataServiceException;
+import org.georchestra.ldapadmin.ds.NotFoundException;
 import org.georchestra.ldapadmin.ds.UserTokenDao;
+import org.georchestra.ldapadmin.dto.Account;
 import org.georchestra.ldapadmin.mailservice.EmailFactoryImpl;
 import org.georchestra.ldapadmin.mailservice.MailService;
 import org.junit.After;
@@ -36,7 +40,9 @@ public class PasswordRecoveryFormControllerTest {
     private UserTokenDao utd = Mockito.mock(UserTokenDao.class);
     private Configuration cfg = new Configuration();
     private ReCaptcha rec = Mockito.mock(ReCaptcha.class);
-
+    private ReCaptchaResponse rer = Mockito.mock(ReCaptchaResponse.class);
+    private Model model = Mockito.mock(Model.class);
+    private HttpServletRequest request = new MockHttpServletRequest();
     PasswordRecoveryFormBean formBean = Mockito.mock(PasswordRecoveryFormBean.class);
     BindingResult result = Mockito.mock(BindingResult.class);
     SessionStatus status = Mockito.mock(SessionStatus.class);
@@ -50,9 +56,17 @@ public class PasswordRecoveryFormControllerTest {
     public void tearDown() throws Exception {
     }
 
-    @Test
-    public void testPasswordRecoveryFormController() {
-
+    private void prepareLegitRequest() throws Exception {
+        request = new MockHttpServletRequest();
+        Mockito.when(formBean.getRecaptcha_challenge_field()).thenReturn("valid");
+        Mockito.when(formBean.getRecaptcha_response_field()).thenReturn("valid");
+        Mockito.when(rer.isValid()).thenReturn(true);
+        Mockito.when(rec.checkAnswer(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+            .thenReturn(rer);
+        Account account = Mockito.mock(Account.class);
+        Mockito.when(account.getUid()).thenReturn("1");
+        Mockito.when(dao.findByEmail(Mockito.anyString())).thenReturn(account);
+        Mockito.when(utd.exist(Mockito.anyString())).thenReturn(true);
     }
 
     @Test
@@ -69,9 +83,8 @@ public class PasswordRecoveryFormControllerTest {
     }
 
     @Test
-    public void testSetupForm() throws IOException {
-        Model model = Mockito.mock(Model.class);
-        HttpServletRequest request = new MockHttpServletRequest();
+    public void testSetupForm() throws Exception {
+        prepareLegitRequest();
 
         String ret = ctrl.setupForm(request , "test@localhost.com", model);
 
@@ -79,8 +92,33 @@ public class PasswordRecoveryFormControllerTest {
     }
 
     @Test
-    public void testGenerateTokenBadEmail() throws IOException {
-        HttpServletRequest request = new MockHttpServletRequest();
+    public void testGenerateTokenWithDataServiceException() throws Exception {
+        prepareLegitRequest();
+        Mockito.when(utd.exist(Mockito.anyString())).thenReturn(false);
+
+        Mockito.doThrow(DataServiceException.class).when(utd).insertToken(Mockito.anyString(), Mockito.anyString());
+
+        try {
+            ctrl.generateToken(request, formBean, result, status);
+        } catch (Throwable e) {
+            assertTrue(e instanceof IOException);
+        }
+
+    }
+
+    @Test
+    public void testGenerateTokenWithUserNotFound() throws Exception {
+        prepareLegitRequest();
+        Mockito.when(utd.exist(Mockito.anyString())).thenReturn(false);
+        Mockito.doThrow(NotFoundException.class).when(dao).findByEmail(Mockito.anyString());
+
+        String ret = ctrl.generateToken(request, formBean, result, status);
+        assertTrue(ret.equals("passwordRecoveryForm"));
+    }
+
+    @Test
+    public void testGenerateTokenBadEmail() throws Exception {
+        prepareLegitRequest();
         Mockito.when(result.hasErrors()).thenReturn(true);
 
         String ret = ctrl.generateToken(request, formBean, result, status);
@@ -89,12 +127,42 @@ public class PasswordRecoveryFormControllerTest {
     }
 
     @Test
-    public void testGenerateToken() throws IOException {
-        HttpServletRequest request = new MockHttpServletRequest();
+    public void testGenerateToken() throws Exception {
+        prepareLegitRequest();
 
-        ctrl.generateToken(request, formBean, result, status);
-
+        String ret = ctrl.generateToken(request, formBean, result, status);
+        assertTrue(ret.equals("emailWasSent"));
     }
 
+    @Test
+    public void testBadCaptchaGenerateToken() throws Exception {
+        prepareLegitRequest();
+        Mockito.when(result.hasErrors()).thenReturn(false, true);
+        Mockito.when(rec.checkAnswer(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+            .thenReturn(rer);
 
+        String ret = ctrl.generateToken(request, formBean, result, status);
+
+        assertTrue(ret.equals("passwordRecoveryForm"));
+    }
+
+    /**
+     * Test the underlying bean (PasswordRecoveryFormBean).
+     *
+     */
+    @Test
+    public void testPasswordRecoveryFormBean() {
+        PasswordRecoveryFormBean b = new PasswordRecoveryFormBean();
+
+        b.setEmail("test@localhost.com");
+        b.setRecaptcha_challenge_field("valid");
+        b.setRecaptcha_response_field("valid");
+
+        assertTrue(b.getRecaptcha_challenge_field().equals(b.getRecaptcha_response_field()));
+        assertTrue(b.getEmail().equals("test@localhost.com"));
+        assertTrue(b.toString().equals("PasswordRecoveryFormBean [email=test@localhost.com, "
+                + "recaptcha_challenge_field=valid, recaptcha_response_field=valid]"));
+
+
+    }
 }
