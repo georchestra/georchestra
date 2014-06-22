f311a20f6c8923ddd4fb046e63f25288771d6dea
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-06-22 23:25:28 +0200

    ldapadmin - adding a test file to test UserTokenDao class
    Not implemented yet.

---
+++ ldapadmin/src/test/java/org/georchestra/ldapadmin/ds/UserTokenDaoTest.java
@@ -0,0 +1,16 @@
+package org.georchestra.ldapadmin.ds;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class UserTokenDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
