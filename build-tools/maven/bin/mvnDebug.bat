291337e0fb05a754b8aa790bf867d439ba8fd514
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-07-02 11:08:17 +0200

    Clean up cas configuration files.
    Removed all properties and configuration files that are not used.

--- build-tools/maven/bin/mvnDebug.bat
+++ build-tools/maven/bin/mvnDebug.bat
@@ -38,6 +38,8 @@
 @REM enable echoing my setting MAVEN_BATCH_ECHO to 'on'
 @if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
 
+SET MAVEN_OPTS=%MAVEN_OPTS% -Xms64m -Xmx1g -XX:PermSize=128m -XX:MaxPermSize=256m
+
 @echo Preparing to Execute Maven in Debug Mode
 
 @REM set %HOME% to equivalent of $HOME
