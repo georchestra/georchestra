b2924aba2b36e72c82c5eee1f0bae6b20c918a64
Davis Mendoza <davis.men.pa@gmail.com>
2014-07-24 10:09:07 -0400

    menu using only javascript

--- header/src/main/webapp/js/header.js
+++ header/src/main/webapp/js/header.js
@@ -1,6 +1,59 @@
-var _headerOnLoad = function(iframe) {
-    var innerDoc = iframe.contentDocument || iframe.contentWindow.document,
-        base = innerDoc.createElement('base');
-    base.setAttribute('target', '_parent');
-    innerDoc.getElementsByTagName('head')[0].appendChild(base);
-};
+var menu = [
+    {id: 'geonetwork', name: 'catalogue', url: '/geonetwork/apps/georchestra/'},
+    {id: 'mapfishapp', name: 'Visualiseur', url: '/mapfishapp/'},
+    {id: 'extractorapp', name: 'Extractor', url: '/extractorapp/'},
+    {id: 'geoserver', name: 'Services', url: '/geoserver/web/'},
+];
+var active;
+
+var req = new XMLHttpRequest();
+req.open('GET', document.location, false);
+req.send(null);
+var headers = req.getAllResponseHeaders().toLowerCase();
+
+var scripts = document.getElementsByTagName('script');
+var myScript = scripts[ scripts.length - 1 ];
+var query = myScript.src.replace(/^[^\?]+\??/, '');
+
+var pairs = query.split(',');
+for (var i = 0; i < pairs.length; i++) {
+    var KeyVal = pairs[i].split('=');
+    if (!KeyVal || KeyVal.length != 2)
+        continue;
+    var key = unescape(KeyVal[0]);
+    var val = unescape(KeyVal[1]);
+    if (key === 'active')
+        active = val;
+}
+
+var logo = '//'+window.location.host+'/IMG/png/logo.png';
+
+document.write('<div id="go_head">');
+document.write('<a id="go_home" href="#">');
+document.write('<img src="img/logo.png" height="50"/>');
+document.write('</a>');
+
+document.write('<ul>');
+for (var i in menu) {
+    document.write("<li>");
+    if (active === menu[i].id) {
+        document.write('<a class="active" href="' + menu[i].url + '" class="menu">' + menu[i].name + '</a>');
+    } else {
+        document.write('<a href="' + menu[i].url + '">' + menu[i].name + '</a>');
+    }
+    document.write('</li>');
+}
+document.write('</ul>');//end ul
+
+if(Boolean(getAnonymous()) == true){
+	document.write('<p class="logged">');
+	document.write('<a href="/ldapadmin/account/userdetails">'+getUserName()+'</a>');
+	document.write('<span class="light"> | </span>');
+	document.write('<a href="/j_spring_security_logout">Logout</a>');
+	document.write('</p>');
+} else {
+	document.write('<p class="logged">');
+	document.write('<a id="login_a">Login</a>');
+	document.write('</p>');
+}
+document.write('</div>'); //end div go_head
