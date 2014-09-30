1aa573765e9a0f66c4da877099c75cea43642eb3
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2014-09-30 16:56:58 +0200

    handling view direction

--- mapfishapp/src/main/webapp/app/addons/streetview/js/main.js
+++ mapfishapp/src/main/webapp/app/addons/streetview/js/main.js
@@ -19,13 +19,17 @@ GEOR.Addons.Streetview.prototype = {
      * record - {Ext.data.record} a record with the addon parameters
      */
     init: function(record) {
-        var style = {
-            externalGraphic: GEOR.config.PATHNAME + "/app/addons/streetview/img/dir_north.png",
-            graphicWidth: 16,
-            graphicHeight: 16,
+        var style = { // TODO: let the admin configure this style
+            graphicWidth: 10,
+            graphicHeight: 10,
             graphicOpacity: 1,
             graphicZIndex: 10000,
-            cursor: "pointer"
+            cursor: "pointer",
+            fillOpacity: 0, 
+            strokeColor: "#67b6f9",
+            strokeOpacity: 0.8,
+            strokeWidth: 3,
+            pointRadius: 3
         };
         this._layer = new OpenLayers.Layer.Vector("addon_streetview_vectors", {
             displayInLayerSwitcher: false,
@@ -38,21 +42,27 @@ GEOR.Addons.Streetview.prototype = {
                     this._updateView();
                     this._window.show();
                 },
-                scope: this
-            }
-        });
-        this._drawControl = new OpenLayers.Control.DrawFeature(this._layer, OpenLayers.Handler.Point, {
-            eventListeners: {
+                "sketchcomplete": function(o) {
+                    // Handling initial point feature.
+                    // change point into regular polygon:
+                    this._createRegularPolygonFromPoint(o.feature.geometry, 0);
+                    // will cause a new "featureadded" event, 
+                    // this time with a regular polygon instead of a point
+                    return false;
+                    // the point feature won't get added to the layer
+                },
                 "featureadded": function(o) {
+                    // Handling regular polygon feature
                     this._drawControl.deactivate();
                     this.map.removeControl(this._drawControl);
-                    this._modifyControl.selectFeature(o.feature);
+                    this._modifyControl.selectFeature(this._layer.features[0]);
                     this._updateView();
                     this._window.show();
                 },
                 scope: this
             }
         });
+        this._drawControl = new OpenLayers.Control.DrawFeature(this._layer, OpenLayers.Handler.Point, {});
         this._modifyControl = new OpenLayers.Control.StreetviewModifyFeature(this._layer, {
             standalone: true,
             mode: OpenLayers.Control.ModifyFeature.ROTATE | 
@@ -108,34 +118,69 @@ GEOR.Addons.Streetview.prototype = {
     },
 
     /**
+     * Method: _onMapZoomend
+     * 
+     */
+    _onMapZoomend: function() {
+        var f = this._layer.features[0];
+        if (!f) {
+            return;
+        }
+        // unselect
+        this._modifyControl.unselectFeature(f);
+        // keep feature geometry apparent diameter
+        this._layer.removeAllFeatures();
+        this._createRegularPolygonFromPoint(f.geometry.getCentroid(), f.geometry.angle);
+        // select again the feature:
+        this._modifyControl.selectFeature(this._layer.features[0]);
+    },
+
+    /**
+     * Method: _createRegularPolygonFromPoint
+     * 
+     */
+    _createRegularPolygonFromPoint: function(geometry, angle) {
+        // radius for 20 px diameter icon size:
+        var radius = 10 * this.map.getResolution();
+        var geom = OpenLayers.Geometry.Polygon.createRegularPolygon(geometry, radius, 20, 0);
+        // we're assuming an initial heading of 0 degrees (North):
+        geom.angle = angle || 0;
+        this._layer.addFeatures([new OpenLayers.Feature.Vector(geom)]);
+    },
+
+    /**
      * Method: _updateView
      * 
      */
     _updateView: function() {
         var feature = this._layer.features[0];
-        if (!feature) { 
+        if (!feature) {
             return;
         }
-        var geom = feature.geometry.clone();
+        var geom = feature.geometry.getCentroid();
         geom.transform(this.map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
         this._window.show();
-        this._window.items.get(0).update('<img src="'+this._buildURL(geom.x, geom.y)+'" />');
+        this._window.items.get(0).update('<img src="'+this._buildURL(geom.x, geom.y, feature.geometry.angle)+'" />');
     },
 
     /**
      * Method: _buildURL
      * 
      */
-    _buildURL: function(lon, lat) {
+    _buildURL: function(lon, lat, angle) {
         var base = "http://maps.googleapis.com/maps/api/streetview?",
             o = this.options,
-            cmp = this._window;
+            cmp = this._window,
+            p = 1e6;
+        if (angle < 0) {
+            angle += 360;
+        }
         return base + OpenLayers.Util.getParameterString({
             key: o.api_key,
-            size: cmp.getInnerWidth()+"x"+cmp.getInnerHeight(),
-            location: lat+","+lon,
+            size: cmp.getInnerWidth() + "x" + cmp.getInnerHeight(),
+            location: Math.round(lat*p)/p + "," + Math.round(lon*p)/p,
             fov: o.initial_fov,
-            heading: 0,
+            heading: Math.round(angle),
             pitch: o.initial_pitch
         });
     },
@@ -153,6 +198,8 @@ GEOR.Addons.Streetview.prototype = {
                 this.map.addControl(this._drawControl);
                 this._drawControl.activate();
             } else {
+                // TODO: update feature geom size ... maybe with:
+                // this._onMapZoomend();
                 this._modifyControl.selectFeature(this._layer.features[0]);
                 this._window.show();
             }
@@ -161,6 +208,10 @@ GEOR.Addons.Streetview.prototype = {
                 GEOR.helper.msg("StreetView", 
                     OpenLayers.i18n("Click on the map to get a glimpse of the surroundings"));
             }
+            this.map.events.on({
+                "zoomend": this._onMapZoomend,
+                scope: this
+            });
         } else {
             this._window.hide();
             if (OpenLayers.Util.indexOf(this.map.controls, this._drawControl) > -1) {
@@ -170,6 +221,10 @@ GEOR.Addons.Streetview.prototype = {
             this._modifyControl.deactivate();
             this.map.removeControl(this._modifyControl);
             this.map.removeLayer(this._layer);
+            this.map.events.un({
+                "zoomend": this._onMapZoomend,
+                scope: this
+            });
         }
     },
 
@@ -178,6 +233,10 @@ GEOR.Addons.Streetview.prototype = {
      * Called by GEOR_tools when deselecting this addon
      */
     destroy: function() {
+        this.map.events.un({
+            "zoomend": this._onMapZoomend,
+            scope: this
+        });
         this._drawControl && this._drawControl.destroy();
         this._drawControl = null;
         this._modifyControl && this._modifyControl.destroy();
