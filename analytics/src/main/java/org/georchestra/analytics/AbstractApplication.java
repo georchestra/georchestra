19f28dee2affc3a38521ed5a33125665d1044c52
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2014-10-06 18:31:29 +0200

    analytics - sanitizing a bit user-provided parameters
    The current code should be looked more thoroughly, I'm not happy with
    the current fix, and I might have forgotten some holes.

--- analytics/src/main/java/org/georchestra/analytics/AbstractApplication.java
+++ analytics/src/main/java/org/georchestra/analytics/AbstractApplication.java
@@ -2,6 +2,8 @@ package org.georchestra.analytics;
 
 import java.io.OutputStream;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,45 +21,60 @@ import org.json.JSONObject;
  *
  */
 public abstract class AbstractApplication {
-	
+
 	protected final Log logger = LogFactory.getLog(getClass().getPackage().getName());
-	
+
 	int month = 1;
 	int year = 2012;
 	int start = 0;
 	int limit = 25;
 	String sort = "count DESC";
 	String filter = "";
-	
+
+    private static List<String> allowedProperties = Arrays.asList("service", "layer", "request", // Layer tab
+            "user_name", // User tab
+            "org", // organization tab
+            "count" // every tab
+    );
+
+    private static List<String> allowedDirections = Arrays.asList("ASC", "DESC");
+
 	/**
 	 * check all the parameters from the request and fill class attributes.
 	 * Return false if any parameters is missing or bad composed.
-	 * 
+	 *
 	 * @param request
 	 * @param msg
 	 * @return
 	 */
 	protected boolean getAllParameters(HttpServletRequest request, StringBuilder msg) {
-		
+
 		try {
 			if(!getDateParameters(request)) return false;
 
 			start = Integer.valueOf(request.getParameter("start"));
 			limit = Integer.valueOf(request.getParameter("limit"));
 			JSONObject obj = new JSONArray(request.getParameter("sort")).getJSONObject(0);
+
+			if (! allowedProperties.contains(obj.getString("property"))
+			        || ! allowedDirections.contains(obj.getString("direction"))) {
+			    throw new IllegalArgumentException("Unexpected parameters provided");
+			}
 			sort = obj.getString("property") + " " + obj.getString("direction");
 			filter = request.getParameter("filter");
-			
+
 		} catch (JSONException e) {
 			msg.append("Error in sort JSON format");
 			return false;
 		} catch (NumberFormatException e) {
 			msg.append("One param is missing");
 			return false;
+		} catch (IllegalArgumentException e) {
+		    return false;
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Check the month and yea parameters and fill the class attributes.
 	 * Return false if one's missing or bad formatted
@@ -65,7 +82,7 @@ public abstract class AbstractApplication {
 	 * @return
 	 */
 	protected boolean getDateParameters(HttpServletRequest request) {
-		
+
 		try {
 			month = Integer.valueOf(request.getParameter("month"));
 			year  = Integer.valueOf(request.getParameter("year"));
@@ -78,12 +95,12 @@ public abstract class AbstractApplication {
 			return false;
 		}
 		return true;
-	}	
-	
+	}
+
 	/**
 	 * Send the unsuccessful response containing success:false, and the message
 	 * from an error
-	 * 
+	 *
 	 * @param out response stream to write the message in
 	 * @param msg error message
 	 * @throws Exception
@@ -94,10 +111,10 @@ public abstract class AbstractApplication {
 		object.put("msg", "invalid params => " + msg);
 		out.write(object.toString().getBytes());
 	}
-	
+
 	/**
 	 * Report an error if exception is thrown during the SQL or JSON process.
-	 * 
+	 *
 	 * @param out response stream to write an error message
 	 * @param response
 	 * @param e
@@ -110,10 +127,10 @@ public abstract class AbstractApplication {
 		logger.error("Caught exception while executing service: ", e);
 		response.setStatus(500);
 	}
-	
+
 	/**
 	 * Update the response ContentType and header to make the navigator download the CSV file
-	 * 
+	 *
 	 * @param csv the CSV content as String
 	 * @param filename the filename the CSV will be saved
 	 * @param response
@@ -123,9 +140,9 @@ public abstract class AbstractApplication {
 		response.setContentType("text/csv");
 		response.setContentLength(csv.getBytes().length);
 		response.setHeader("Content-Disposition", "attachment; filename=\""+filename+CSVUtil.CSV_EXT+"\"");
-		response.getWriter().write(csv); 
+		response.getWriter().write(csv);
 	}
-	
+
 	/**
 	 * Generic method from all WS. Will call the stretegy.process method which refer to the
 	 * WS' model. This model will return result as JSONObject. This object will be return
@@ -133,37 +150,37 @@ public abstract class AbstractApplication {
 	 * number of result, and a array of results.
 	 * If an error is thrown during the request, a JSON response with success:false will be return.
 	 * If an error occurs during SQL or JSON process, an exception will be returned.
-	 * 
+	 *
 	 * @param request
 	 * @param response
 	 * @param strategy contain the method to call the model an retrieve results
 	 * @throws Exception
 	 */
 	protected void getStats(HttpServletRequest request, HttpServletResponse response, StrategyController strategy) throws Exception {
-		
+
 		OutputStream out  = response.getOutputStream();
 		StringBuilder msg = new StringBuilder();
-		
+
 		if(!this.getAllParameters(request, msg)) {
 			sendSuccessFalse(out, msg.toString());
 		}
 		else {
 			try {
 				JSONObject object  = strategy.process();
-				out.write(object.toString().getBytes());		
-				
+				out.write(object.toString().getBytes());
+
 			} catch (Exception e) {
 				reportError(out, response, e);
-			} 
+			}
 		}
 	}
-	
+
 	/**
 	 * Generic method from all WS. Will call the stretegy.process method which refer to the
 	 * WS' model. This model will return result as JSONObject. This object will then be
 	 * parsed to write a CSV file which be return in the response, using response header as
 	 * CSV file.
-	 * 
+	 *
 	 * @param request
 	 * @param response
 	 * @param strategy contain the method to call the model an retrieve results
@@ -181,7 +198,7 @@ public abstract class AbstractApplication {
 				JSONObject object  = strategy.process();
 				csv = CSVUtil.JSONToCSV(object);
 				csvFileName = String.valueOf(year) + "-" + String.format("%02d", month) + "-" + csvFileName;
-		
+
 			} catch (Exception e) {
 				OutputStream out  = response.getOutputStream();
 				reportError(out, response, e);
@@ -189,14 +206,14 @@ public abstract class AbstractApplication {
 			respondCSV(csv, csvFileName, response);
 		}
 	}
-	
+
 	/**
 	 * Inner Abstract class to pass the call of the specific model to the generic
 	 * getStats and exportCSV methods.
 	 *
 	 */
 	protected abstract class StrategyController {
-		
+
 		protected abstract JSONObject process() throws SQLException, JSONException;
 	}
 }
--- analytics/src/main/java/org/georchestra/analytics/model/OGCStatsModel.java
+++ analytics/src/main/java/org/georchestra/analytics/model/OGCStatsModel.java
@@ -12,24 +12,61 @@ public class OGCStatsModel extends AbstractModel  {
 	public OGCStatsModel(PostGresqlConnection pgpool) {
 		super(pgpool);
 	}
-	
-	private final String selectLayersQ = "SELECT service, layer, request, count(*) as count FROM ogcstatistics.ogc_services_log " +
-			"where extract(month from date) = ? AND extract(year from date) = ? " +
-			"group by layer, service,request order by @sort@ LIMIT ? OFFSET ?;";
-	
-	private final String selectUsersQ = "SELECT user_name, count(*) as count FROM ogcstatistics.ogc_services_log " +
-			"where extract(month from date) = ? AND extract(year from date) = ? " +
-			"group by user_name order by @sort@ LIMIT ? OFFSET ?;";
-	
-	private final String selectGroupsQ = "SELECT org, count(*) as count FROM ogcstatistics.ogc_services_log " +
-			"where extract(month from date) = ? AND extract(year from date) = ? " +
-			"group by org order by @sort@ LIMIT ? OFFSET ?;";
-	
+
+	private final String selectLayersQ = "SELECT "
+	        + "    service"
+	        + "    , layer"
+	        + "    , request"
+	        + "    , COUNT(*) AS count "
+	        + "FROM "
+	        + "    ogcstatistics.ogc_services_log "
+	        + "WHERE "
+	        + "    extract(month from date) = ? "
+	        + "AND "
+	        + "    extract(year from date) = ? "
+	        + "GROUP BY "
+	        + "    layer"
+	        + "    , service"
+	        + "    , request "
+	        + "ORDER BY "
+	        + "    @sort@ "
+	        + "LIMIT ? OFFSET ?;";
+
+	private final String selectUsersQ = "SELECT "
+	        + "    user_name"
+	        + "    , COUNT(*) AS count "
+	        + "FROM "
+	        + "    ogcstatistics.ogc_services_log "
+	        +"WHERE "
+	        + "    extract(month from date) = ? "
+	        + "AND "
+	        + "    extract(year from date) = ? "
+	        + "GROUP BY "
+	        + "    user_name "
+	        + "ORDER BY "
+	        + "    @sort@ "
+	        + "LIMIT ? OFFSET ?;";
+
+	private final String selectGroupsQ = "SELECT "
+	        + "    org"
+	        + "    , COUNT(*) AS count "
+	        + "FROM "
+	        + "    ogcstatistics.ogc_services_log "
+	        + "WHERE "
+	        + "    extract(month from date) = ? "
+	        + "AND "
+	        + "    extract(year from date) = ? "
+	        + "GROUP BY "
+	        + "    org "
+	        + "ORDER BY "
+	        + "    @sort@ "
+	        + "LIMIT ? OFFSET ?;";
+
 	public JSONObject getLayersStats(final int month, final int year, final int start, final int limit, final String sort, final String filter) throws SQLException, JSONException {
-		
+
 		return getStats(month, year, start, limit, sort, filter, selectLayersQ, new StrategyModel() {
 
-			protected JSONArray process(ResultSet rs) throws SQLException, JSONException {	
+			protected JSONArray process(ResultSet rs) throws SQLException, JSONException {
 				JSONArray jsarr = new JSONArray();
 				while (rs.next()) {
 					JSONObject res = new JSONObject();
@@ -38,39 +75,39 @@ public class OGCStatsModel extends AbstractModel  {
 					res.put("request", rs.getString("request"));
 					res.put("count", rs.getInt("count"));
 					jsarr.put(res);
-			     }	
+			     }
 				return jsarr;
 			}
 		});
 	}
-	
+
 	public JSONObject getUsersStats(final int month, final int year, final int start, final int limit, final String sort, final String filter) throws SQLException, JSONException {
-		
+
 		return getStats(month, year, start, limit, sort, filter, selectUsersQ, new StrategyModel() {
-			protected JSONArray process(ResultSet rs) throws SQLException, JSONException {	
+			protected JSONArray process(ResultSet rs) throws SQLException, JSONException {
 				JSONArray jsarr = new JSONArray();
 				while (rs.next()) {
 					JSONObject res = new JSONObject();
 					res.put("user_name", rs.getString("user_name"));
 					res.put("count", rs.getInt("count"));
 					jsarr.put(res);
-			     }	
+			     }
 				return jsarr;
 			}
 		});
 	}
-	
+
 	public JSONObject getGroupsStats(final int month, final int year, final int start, final int limit, final String sort, final String filter) throws SQLException, JSONException {
-		
+
 		return getStats(month, year, start, limit, sort, filter, selectGroupsQ, new StrategyModel() {
-			protected JSONArray process(ResultSet rs) throws SQLException, JSONException {	
+			protected JSONArray process(ResultSet rs) throws SQLException, JSONException {
 				JSONArray jsarr = new JSONArray();
 				while (rs.next()) {
 					JSONObject res = new JSONObject();
 					res.put("org", rs.getString("org"));
 					res.put("count", rs.getInt("count"));
 					jsarr.put(res);
-			     }	
+			     }
 				return jsarr;
 			}
 		});
