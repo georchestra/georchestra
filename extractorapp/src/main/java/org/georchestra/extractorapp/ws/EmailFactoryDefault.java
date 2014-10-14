5e5bcf91a8b21730c0a91fa0d6bede727e269767
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2014-10-14 13:19:45 +0200

    extractorapp - Improve mail templating system (#750)
    Implements the possibility to add custom key/value pairs replacements in
    the templating mail system.
    
    Manually tested.
    
    Conflicts:
    	analytics/src/main/webapp/js/lib/external/ext

--- extractorapp/src/main/java/org/georchestra/extractorapp/ws/EmailFactoryDefault.java
+++ extractorapp/src/main/java/org/georchestra/extractorapp/ws/EmailFactoryDefault.java
@@ -2,6 +2,7 @@ package org.georchestra.extractorapp.ws;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.mail.MessagingException;
@@ -9,14 +10,15 @@ import javax.mail.internet.AddressException;
 import javax.servlet.http.HttpServletRequest;
 
 public class EmailFactoryDefault extends AbstractEmailFactory {
-	
+
 	@Override
 	public Email createEmail(HttpServletRequest request, final String[] recipients, final String url) throws IOException {
-		
+
 		final long expiry = this.expireDeamon.getExpiry();
 		final String msgAck = readFile(request, emailAckTemplateFile);
 		final String msgDone = readFile(request, emailTemplateFile);
-		
+		final HashMap<String,String> extraKeywords = readExtraKeywords(extraKeywordsFile);
+
 		return new Email(request, recipients, emailSubject,
 				this.smtpHost,
 				this.smtpPort,
@@ -29,20 +31,34 @@ public class EmailFactoryDefault extends AbstractEmailFactory {
 		) {
 			public void sendDone(List<String> successes, List<String> failures,
 		            List<String> oversized, long fileSize) throws MessagingException {
-				
+
+			    String globalStatus = "";
+			    if (successes.size() > 0 && failures.size() > 0)
+			        globalStatus = extraKeywords.get("partial_success");
+			    else if (successes.size() > 0 && failures.size() <= 0)
+                    globalStatus = extraKeywords.get("success");
+			    else if (successes.size() <= 0 && failures.size() > 0)
+                    globalStatus = extraKeywords.get("failure");
+
 		        LOG.debug("preparing to send extraction done email");
 		        String msg = new String(msgDone);
 		        if (msg != null) {
 		            msg = msg.replace("{link}", url);
 		            msg = msg.replace("{emails}", Arrays.toString(recipients));
 		            msg = msg.replace("{expiry}", String.valueOf(expiry));
-		            msg = msg.replace("{successes}", format(successes));
-		            msg = msg.replace("{failures}", format(failures));
-		            msg = msg.replace("{oversized}", format(oversized));
+		            msg = msg.replace("{successes}", format(successes, "{success}"));
+		            msg = msg.replace("{failures}", format(failures, "{failure}"));
+		            msg = msg.replace("{oversized}", format(oversized, "{failure_oversized}"));
+
+		            // extra parameters
+		            msg = msg.replace("{global_status}", globalStatus);
+		            for (String key: extraKeywords.keySet()) {
+		                  msg = msg.replace(String.format("{%s}", key), extraKeywords.get(key));
+		            }
 		        }
 		        sendMsg(msg);
 		    }
-		    
+
 		    public void sendAck() throws AddressException, MessagingException {
 		        LOG.debug("preparing to send extraction ack email");
 				sendMsg(msgAck);
