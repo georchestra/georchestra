0c4a02fc40d48eebb7a74d6769631f1cee119437
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2014-10-16 12:32:28 +0200

    extractorapp - reindent

--- extractorapp/src/main/java/org/georchestra/extractorapp/ws/EmailFactoryDefault.java
+++ extractorapp/src/main/java/org/georchestra/extractorapp/ws/EmailFactoryDefault.java
@@ -11,59 +11,58 @@ import javax.servlet.http.HttpServletRequest;
 
 public class EmailFactoryDefault extends AbstractEmailFactory {
 
-	@Override
-	public Email createEmail(HttpServletRequest request, final String[] recipients, final String url) throws IOException {
+    @Override
+    public Email createEmail(HttpServletRequest request,
+            final String[] recipients, final String url) throws IOException {
 
-		final long expiry = this.expireDeamon.getExpiry();
-		final String msgAck = readFile(request, emailAckTemplateFile);
-		final String msgDone = readFile(request, emailTemplateFile);
-		final HashMap<String,String> extraKeywords = readExtraKeywords(extraKeywordsFile);
+        final long expiry = this.expireDeamon.getExpiry();
+        final String msgAck = readFile(request, emailAckTemplateFile);
+        final String msgDone = readFile(request, emailTemplateFile);
+        final HashMap<String, String> extraKeywords = readExtraKeywords(extraKeywordsFile);
 
-		return new Email(request, recipients, emailSubject,
-				this.smtpHost,
-				this.smtpPort,
-				this.emailHtml,
-				this.replyTo,
-				this.from,
-				this.bodyEncoding,
-				this.subjectEncoding,
-				this.languages
-		) {
-			public void sendDone(List<String> successes, List<String> failures,
-		            List<String> oversized, long fileSize) throws MessagingException {
+        return new Email(request, recipients, emailSubject, this.smtpHost,
+                this.smtpPort, this.emailHtml, this.replyTo, this.from,
+                this.bodyEncoding, this.subjectEncoding, this.languages) {
+            public void sendDone(List<String> successes, List<String> failures,
+                    List<String> oversized, long fileSize)
+                    throws MessagingException {
 
-			    String globalStatus = "";
-			    if (successes.size() > 0 && failures.size() > 0)
-			        globalStatus = extraKeywords.get("partial_success");
-			    else if (successes.size() > 0 && failures.size() <= 0)
+                String globalStatus = "";
+                if (successes.size() > 0 && failures.size() > 0)
+                    globalStatus = extraKeywords.get("partial_success");
+                else if (successes.size() > 0 && failures.size() <= 0)
                     globalStatus = extraKeywords.get("success");
-			    else if (successes.size() <= 0 && failures.size() > 0)
+                else if (successes.size() <= 0 && failures.size() > 0)
                     globalStatus = extraKeywords.get("failure");
 
-		        LOG.debug("preparing to send extraction done email");
-		        String msg = new String(msgDone);
-		        if (msg != null) {
-		            msg = msg.replace("{link}", url);
-		            msg = msg.replace("{emails}", Arrays.toString(recipients));
-		            msg = msg.replace("{expiry}", String.valueOf(expiry));
-		            msg = msg.replace("{successes}", format(successes, "{success}"));
-		            msg = msg.replace("{failures}", format(failures, "{failure}"));
-		            msg = msg.replace("{oversized}", format(oversized, "{failure_oversized}"));
+                LOG.debug("preparing to send extraction done email");
+                String msg = new String(msgDone);
+                if (msg != null) {
+                    msg = msg.replace("{link}", url);
+                    msg = msg.replace("{emails}", Arrays.toString(recipients));
+                    msg = msg.replace("{expiry}", String.valueOf(expiry));
+                    msg = msg.replace("{successes}",
+                            format(successes, "{success}"));
+                    msg = msg.replace("{failures}",
+                            format(failures, "{failure}"));
+                    msg = msg.replace("{oversized}",
+                            format(oversized, "{failure_oversized}"));
 
-		            // extra parameters
-		            msg = msg.replace("{global_status}", globalStatus);
-		            for (String key: extraKeywords.keySet()) {
-		                  msg = msg.replace(String.format("{%s}", key), extraKeywords.get(key));
-		            }
-		        }
-		        sendMsg(msg);
-		    }
+                    // extra parameters
+                    msg = msg.replace("{global_status}", globalStatus);
+                    for (String key : extraKeywords.keySet()) {
+                        msg = msg.replace(String.format("{%s}", key),
+                                extraKeywords.get(key));
+                    }
+                }
+                sendMsg(msg);
+            }
 
-		    public void sendAck() throws AddressException, MessagingException {
-		        LOG.debug("preparing to send extraction ack email");
-				sendMsg(msgAck);
-			}
-		};
-	}
+            public void sendAck() throws AddressException, MessagingException {
+                LOG.debug("preparing to send extraction ack email");
+                sendMsg(msgAck);
+            }
+        };
+    }
 
 }
