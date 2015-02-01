f2c0e41f5b157ccad6f60ee8cd6651cc89d84039
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-02-01 15:28:03 +0100

    ldapadmin - repairing testsuite

--- ldapadmin/src/test/java/org/georchestra/ldapadmin/ds/QueryUserTokenExpiredCommandTest.java
+++ ldapadmin/src/test/java/org/georchestra/ldapadmin/ds/QueryUserTokenExpiredCommandTest.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.georchestra.lib.sqlcommand.DataCommandException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
