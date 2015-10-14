eba1a800979496859769965db8c9817cbef0b822
Vincent Dorut <vincent.dorut@camptocamp.com>
2015-10-14 10:51:00 +0200

    Export Users as CSV / VCF
    - Removing super-csv, generating by hand instead
    - Adding tests
    - Testing against real ldap

--- ldapadmin/src/main/java/org/georchestra/ldapadmin/dto/AccountImpl.java
+++ ldapadmin/src/main/java/org/georchestra/ldapadmin/dto/AccountImpl.java
@@ -5,6 +5,7 @@ package org.georchestra.ldapadmin.dto;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 
 import ezvcard.VCard;
@@ -91,7 +92,8 @@ public class AccountImpl implements Serializable, Account, Comparable<Account>{
 				+ organizationalUnit + ", homePostalAddres=" + homePostalAddres
 				+ "]";
 	}
-
+	
+	
 	@Override
 	public String toVcf() {
 	    VCard v = new VCard();
@@ -114,11 +116,133 @@ public class AccountImpl implements Serializable, Account, Comparable<Account>{
 	    return v.write();
 	}
 
+	
+	
+	
+	
+
+	
+	
+	private final String CSV_DELIMITER = ",";
+	
+	
 	@Override
 	public String toCsv() {
-		return null;
-	}
-
+        
+    StringBuilder csv  = new StringBuilder ();
+    
+    csv.append(StringUtils.isEmpty(commonName) ? "" : commonName);
+    csv.append(CSV_DELIMITER);
+    csv.append(CSV_DELIMITER);//Middle Name
+    csv.append(StringUtils.isEmpty(surname) ? "" : surname);
+    csv.append(CSV_DELIMITER);
+    csv.append(StringUtils.isEmpty(title) ? "" : title);
+    csv.append(CSV_DELIMITER); 
+    csv.append(CSV_DELIMITER);//Suffix
+    csv.append(CSV_DELIMITER); //Initials
+    csv.append(CSV_DELIMITER);//Web Page
+    csv.append(CSV_DELIMITER); //Gender
+    csv.append(CSV_DELIMITER);//Birthday
+    csv.append(CSV_DELIMITER); //Anniversary
+    csv.append(CSV_DELIMITER);//Location
+    csv.append(CSV_DELIMITER); //Language
+    csv.append(CSV_DELIMITER);//Internet Free Busy
+    csv.append(CSV_DELIMITER); //Notes
+    csv.append(StringUtils.isEmpty(email) ? "" : email);
+    csv.append(CSV_DELIMITER);
+    csv.append(CSV_DELIMITER);//E-mail 2 Address
+    csv.append(CSV_DELIMITER); //E-mail 3 Address
+    csv.append(StringUtils.isEmpty(phone) ? "" : phone);// primary phone
+    csv.append(CSV_DELIMITER); 
+    csv.append(CSV_DELIMITER);//Home Phone
+    csv.append(CSV_DELIMITER); //Home Phone 2
+    csv.append(StringUtils.isEmpty(mobile) ? "" : mobile);
+    csv.append(CSV_DELIMITER); //Mobile Phone
+    csv.append(CSV_DELIMITER);//Pager
+    csv.append(CSV_DELIMITER);//Home Fax
+    csv.append(StringUtils.isEmpty(homePostalAddres) ? "" : homePostalAddres);
+    csv.append(CSV_DELIMITER);//Home Address
+    csv.append(CSV_DELIMITER);//Home Street
+    csv.append(CSV_DELIMITER);//Home Street 2
+    csv.append(CSV_DELIMITER);//Home Street 3
+    csv.append(CSV_DELIMITER);//Home Address PO Box
+    csv.append(CSV_DELIMITER); // locality
+    csv.append(CSV_DELIMITER); //Home City
+    csv.append(CSV_DELIMITER);//Home State
+    csv.append(CSV_DELIMITER); //Home Postal Code
+    csv.append(CSV_DELIMITER);// Home Country
+    csv.append(CSV_DELIMITER);//Spouse
+    csv.append(CSV_DELIMITER);//Children
+    csv.append(CSV_DELIMITER); //Manager's Name
+    csv.append(CSV_DELIMITER);// Assistant's Name
+    csv.append(CSV_DELIMITER); //Referred By
+    csv.append(CSV_DELIMITER);//Company Main Phone
+    csv.append(CSV_DELIMITER);//Business Phone
+    csv.append(CSV_DELIMITER);//Business Phone 2
+    csv.append(StringUtils.isEmpty(facsimile) ? "" : facsimile);
+    csv.append(CSV_DELIMITER); //Business Fax
+    csv.append(CSV_DELIMITER);//Assistant's Phone
+    csv.append(StringUtils.isEmpty(org) ? "" : org);
+    csv.append(CSV_DELIMITER); //Company
+    csv.append(StringUtils.isEmpty(description) ? "" : description);
+    csv.append(CSV_DELIMITER);//Job Title
+    csv.append(CSV_DELIMITER);//Department
+    csv.append(CSV_DELIMITER);//Office Location
+    csv.append(CSV_DELIMITER);// Organizational ID Number
+    csv.append(CSV_DELIMITER);//Profession
+    csv.append(CSV_DELIMITER); //Account
+    csv.append(StringUtils.isEmpty(postalAddress) ? "" : postalAddress);
+    csv.append(CSV_DELIMITER);//Business Address
+    csv.append(StringUtils.isEmpty(street) ? "" : street);
+    csv.append(CSV_DELIMITER);// Business Street
+    csv.append(CSV_DELIMITER);//Business Street 2
+    csv.append(CSV_DELIMITER); //Business Street 3
+    csv.append(StringUtils.isEmpty(postOfficeBox) ? "" : postOfficeBox);
+    csv.append(CSV_DELIMITER);//Business Address PO Box
+    csv.append(CSV_DELIMITER);// Business City
+    csv.append(CSV_DELIMITER);//Business State
+    csv.append(StringUtils.isEmpty(postalCode) ? "" : postalCode);
+    csv.append(CSV_DELIMITER); //Business Postal Code
+    csv.append(StringUtils.isEmpty(stateOrProvince) ? "" : stateOrProvince);
+    csv.append(CSV_DELIMITER);//Business Country
+    csv.append(CSV_DELIMITER);// Other Phone
+    csv.append(CSV_DELIMITER);//Other Fax
+    csv.append(StringUtils.isEmpty(registeredAddress) ? "" : registeredAddress);
+    csv.append(CSV_DELIMITER); //Other Address
+    csv.append(StringUtils.isEmpty(physicalDeliveryOfficeName) ? "" : physicalDeliveryOfficeName);
+    csv.append(CSV_DELIMITER);//Other Street
+    csv.append(CSV_DELIMITER);//Other Street 2
+    csv.append(CSV_DELIMITER);// Other Street 3
+    csv.append(CSV_DELIMITER);//Other Address PO Box
+    csv.append(CSV_DELIMITER); //Other City
+    csv.append(CSV_DELIMITER);//Other State
+    csv.append(CSV_DELIMITER);// Other Postal Code
+    csv.append(CSV_DELIMITER);//Other Country
+    csv.append(CSV_DELIMITER); //Callback
+    csv.append(CSV_DELIMITER);//Car Phone
+    csv.append(CSV_DELIMITER);//ISDN
+    csv.append(CSV_DELIMITER);//Radio Phone 
+    csv.append(CSV_DELIMITER);//TTY/TDD Phone
+    csv.append(CSV_DELIMITER); //Telex
+    csv.append(CSV_DELIMITER);//User 1
+    csv.append(CSV_DELIMITER);// User 2
+    csv.append(CSV_DELIMITER);//User 3
+    csv.append(CSV_DELIMITER); //User 4
+    csv.append(CSV_DELIMITER);//Keywords
+    csv.append(CSV_DELIMITER);//Mileage
+    csv.append(CSV_DELIMITER);// Hobby
+    csv.append(CSV_DELIMITER);//Billing Information
+    csv.append(CSV_DELIMITER); //Directory Server
+    csv.append(CSV_DELIMITER);//Sensitivity
+    csv.append(CSV_DELIMITER);// Priority
+    csv.append(CSV_DELIMITER);//Private
+    csv.append(CSV_DELIMITER); //Categories
+    csv.append("\r\n"); // CRLF
+    return csv.toString();
+    		
+    };
+            
+    
 	@Override
 	public void setUid(String uid) {
 		this.uid = uid;
--- ldapadmin/src/main/java/org/georchestra/ldapadmin/ws/backoffice/users/UsersExport.java
+++ ldapadmin/src/main/java/org/georchestra/ldapadmin/ws/backoffice/users/UsersExport.java
@@ -1,5 +1,7 @@
 package org.georchestra.ldapadmin.ws.backoffice.users;
 
+import java.io.StringWriter;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.georchestra.ldapadmin.ds.AccountDao;
@@ -12,26 +14,73 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.supercsv.cellprocessor.FmtBool;
+import org.supercsv.cellprocessor.FmtDate;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.constraint.LMinMax;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 @Controller
 public class UsersExport {
 
     private AccountDao accountDao;
 
-    private static final Log LOG = LogFactory.getLog(UsersController.class.getName());
+    // Used for testing only
+    public void setAccountDao(AccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+
+
+
+
+	private static final Log LOG = LogFactory.getLog(UsersController.class.getName());
+    
+
+    // Outlook csv header
+    private final String OUTLOOK_CSV_HEADER = "First Name,Middle Name,Last Name,Title,Suffix,Initials,Web Page,Gender,Birthday,Anniversary,"
+    		+ "Location,Language,Internet Free Busy,Notes,E-mail Address,E-mail 2 Address,E-mail 3 Address,Primary Phone,Home Phone,"
+    		+ "Home Phone 2,Mobile Phone,Pager,Home Fax,Home Address,Home Street,Home Street 2,Home Street 3,Home Address PO Box,Home City,"
+    		+ "Home State,Home Postal Code,Home Country,Spouse,Children,Manager's Name,Assistant's Name,Referred By,Company Main Phone,"
+    		+ "Business Phone,Business Phone 2,Business Fax,Assistant's Phone,Company,Job Title,Department,Office Location,Organizational ID Number,"
+    		+ "Profession,Account,Business Address,Business Street,Business Street 2,Business Street 3,Business Address PO Box,Business City,"
+    		+ "Business State,Business Postal Code,Business Country,Other Phone,Other Fax,Other Address,Other Street,Other Street 2,Other Street 3,"
+    		+ "Other Address PO Box,Other City,Other State,Other Postal Code,Other Country,Callback,Car Phone,ISDN,Radio Phone,TTY/TDD Phone,Telex,"
+    		+ "User 1,User 2,User 3,User 4,Keywords,Mileage,Hobby,Billing Information,Directory Server,Sensitivity,Priority,Private,Categories\r\n";
+    
 
     @Autowired
     public UsersExport(AccountDao dao) {
         this.accountDao = dao;
     }
+    
+    
 
     @RequestMapping(value = "/private/users.csv", method = RequestMethod.POST,
             produces = "text/csv; charset=utf-8")
     @ResponseBody
     public String getUsersAsCsv(@RequestBody String users) throws Exception {
         JSONArray jsonUsers = new JSONArray(users);
-        return jsonUsers.toString(4);
+        StringBuilder ret1 = new StringBuilder();
+        ret1.append(OUTLOOK_CSV_HEADER); // add csv outlook header
+        
+        for(int i = 0; i < jsonUsers.length(); ++ i){
+            try {
+                Account a = accountDao.findByUID(jsonUsers.getString(i));
+                ret1.append(a.toCsv());
+            } catch (NotFoundException e) {
+                LOG.error(String.format("User [%s] not found, skipping", jsonUsers.getString(i)), e);
+            }
+        }
+
+        return ret1.toString();
     }
+    
+
+    
 
     @RequestMapping(value = "private/users.vcf", method = RequestMethod.POST,
             produces = "text/x-vcard; charset=utf-8")
@@ -51,5 +100,9 @@ public class UsersExport {
         return ret.toString();
     }
 
+    
+    
+    
+    
 
 }
