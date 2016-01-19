fdd2293e856a10d2d35dd517b94db36915cfd697
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2016-01-19 13:02:31 +0100

    ldapadmin - repairing Java build

--- ldapadmin/src/main/java/org/georchestra/ldapadmin/ws/backoffice/users/UsersController.java
+++ ldapadmin/src/main/java/org/georchestra/ldapadmin/ws/backoffice/users/UsersController.java
@@ -51,7 +51,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.NameNotFoundException;
 import org.springframework.ldap.filter.LikeFilter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Web Services to maintain the User information.
@@ -119,7 +123,6 @@ public class UsersController {
 	 * @param response
 	 * @throws IOException
 	 */
-	@CrossOrigin(methods = RequestMethod.OPTIONS)
 	@RequestMapping(value=REQUEST_MAPPING, method=RequestMethod.GET)
 	public void findAll( HttpServletRequest request, HttpServletResponse response ) throws IOException{
 		List protectedUsers = this.userRule.getListUidProtected();
