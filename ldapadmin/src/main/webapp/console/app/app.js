1d2abd09feceed2cd1470e73b840fcf5a982ca96
Antoine Abt <antoine@abt.im>
2016-02-09 13:27:29 +0100

    Fix template cache build with brunch

--- ldapadmin/src/main/webapp/console/app/app.js
+++ ldapadmin/src/main/webapp/console/app/app.js
@@ -12,22 +12,30 @@ angular.module('admin_console', [
   'LDAP_BASE_URI', '/ldapadmin/private/'
 ).constant(
   'ANALYTICS_BASE_URI', 'http://localhost:8280/analytics/ws/'
-).config(['$translateProvider', function ($translateProvider) {
+).config(['$componentLoaderProvider', function ($componentLoaderProvider) {
+  $componentLoaderProvider.setTemplateMapping(function (name) {
+    return 'components/' + name + '/' + name + '.tpl.html';
+  });
+}]).config(['$translateProvider', function ($translateProvider) {
 
   $translateProvider
     .preferredLanguage('en')
     .useSanitizeValueStrategy('escape')
     .useStaticFilesLoader({
-      prefix: '../public/lang/',
+      prefix: 'lang/',
       suffix: '.json'
     });
 
 }]);
 
 require('./components/users/users');
+require('./components/users/users.tpl');
 require('./components/groups/groups');
+require('./components/groups/groups.tpl');
 require('./components/user/user');
+require('./components/user/user.tpl');
 require('./components/home/home');
+require('./components/home/home.tpl');
 require('./services/analytics');
 require('./services/groups');
 require('./services/groups_users');
