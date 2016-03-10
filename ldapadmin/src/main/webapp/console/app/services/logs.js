be200140cc8629032e45e84a4324ea53bb2785dc
Antoine Abt <antoine@abt.im>
2016-03-10 11:34:12 +0100

    Plug some home service & swich controller to es6 classes :sunglasses:

--- ldapadmin/src/main/webapp/console/app/services/logs.js
+++ ldapadmin/src/main/webapp/console/app/services/logs.js
@@ -1,6 +1,6 @@
 angular.module('admin_console').factory('Logs',
-  ['$resource', 'ANALYTICS_BASE_URI', function($resource, baseUri) {
-    return $resource(baseUri + 'private/admin_logs', {}, {
+  ['$resource', 'LDAP_BASE_URI', function($resource, baseUri) {
+    return $resource(baseUri + 'admin_logs/:limit/:page', {}, {
       query     : {
         method  : 'GET',
         cache   : true,
