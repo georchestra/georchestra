6fba17f85405c324a1a720800c322fed9ebd795e
Pierre Mauduit <pierre.mauduit@gmail.com>
2016-03-10 20:19:42 +0100

    WMC roles / datadir - refactor @jusabatier's work
    - Splitting the roles calculation into 2 methods
    - Adding some unit tests to the testsuite

--- mapfishapp/src/main/java/org/georchestra/mapfishapp/ws/ContextController.java
+++ mapfishapp/src/main/java/org/georchestra/mapfishapp/ws/ContextController.java
@@ -29,6 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
@@ -80,7 +81,7 @@ public class ContextController implements ServletContextAware {
         // defaulting to default.png
         String image = "context/image/default.png";
         // roles
-        JSONArray roles = new JSONArray();
+        JSONArray roles = getRolesForContext(pathCtx, title);
 
         File imagePath = new File(pathCtx, "images");
         if (! imagePath.isDirectory()) {
@@ -94,30 +95,6 @@ public class ContextController implements ServletContextAware {
                 }
             }
         }
-        
-        File rolePath = new File(pathCtx);
-        Collection<File> files = FileUtils.listFiles(rolePath, new String[] { "XML", "xml" }, false);
-        for (File curRoleFile : files) {
-            if (FilenameUtils.getBaseName(curRoleFile.toString()).equalsIgnoreCase(title)) {
-                DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = domFactory.newDocumentBuilder();
-                Document doc = builder.parse(curRoleFile);
-                XPath xpath = XPathFactory.newInstance().newXPath();
-                
-                // Parsing roles
-                XPathExpression xpRole = xpath.compile("//AllowedRoles/Role");
-                Object oRole = xpRole.evaluate(doc, XPathConstants.NODESET);
-                
-                if (oRole instanceof NodeList) {
-                    NodeList nl = (NodeList) oRole;
-                    for (int i = 0; i < nl.getLength(); ++i) {
-                        roles.put(nl.item(i).getTextContent());
-                    }
-                }
-                break;
-            }
-        }
-        
         // filename
         String wmcUrl = "context/" + f.getName();
 
@@ -132,6 +109,82 @@ public class ContextController implements ServletContextAware {
         info.put("roles", roles);
 
         return info;
+    }
+
+    /**
+     * Gets the roles as a JSON array for the given context. If a XML file is
+     * provided next to the context file (i.e. context.xml and context.wmc), it
+     * is possible to limit its access to the defined roles.
+     *
+     * The xml file should have the following syntax:
+     *
+     * <pre>
+     *   <?xml version="1.0" encoding="UTF-8"?>
+     *   <AllowedRoles>
+     *        <Role>ROLE_ADMINISTRATOR</Role>
+     *        <Role>ROLE_SER_URBANISME</Role>
+     *        <Role>ROLE_SER_INGENIERIE</Role>
+     *        <Role>ROLE_PERM_COMMUNES</Role>
+     *   </AllowedRoles>
+     * </pre>
+     *
+     * The purpose is not to prevent the user from accessing the context (since
+     * if the user is knowing the context name, it is trivial to call the
+     * controller and get the context served by the getContext() method below),
+     * but to at least hide it from the mapfishapp interface.
+     *
+     * @param pathContext
+     *            the path to the WMC context file
+     * @param title
+     *            the title
+     * @return a JSON array containing the roles, or an empty array if all roles
+     *         are allowed.
+     * @throws Exception
+     */
+    private JSONArray getRolesForContext(String pathContext, String title) {
+        JSONArray roles = new JSONArray();
+        File rolePath = new File(pathContext);
+        Collection<File> files = FileUtils.listFiles(rolePath, new String[] { "XML", "xml" }, false);
+        // TODO: instead of iterating on each XML files from the directory, it
+        // might be more relevant to consider using a file with the same
+        // basename as the WMC ?
+        for (File curRoleFile : files) {
+            if (FilenameUtils.getBaseName(curRoleFile.toString()).equalsIgnoreCase(title)) {
+                try {
+                    roles = parseRoleXmlFile(curRoleFile);
+                } catch (Exception e) {
+                    LOG.error("Error parsing role file: " + curRoleFile, e);
+                }
+                break;
+            }
+        }
+        return roles;
+    }
+
+    /**
+     * Parses a single XML file containing the roles for a given context.
+     *
+     * @param roleXml the file to be parsed
+     * @return a JSON array containing the roles, or an empty array if all roles are allowed.
+     * @throws Exception
+     */
+    private JSONArray parseRoleXmlFile(File roleXml) throws Exception {
+        JSONArray roles = new JSONArray();
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+        Document doc = builder.parse(roleXml);
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        // Parsing roles
+        XPathExpression xpRole = xpath.compile("//AllowedRoles/Role");
+        Object oRole = xpRole.evaluate(doc, XPathConstants.NODESET);
+
+        if (oRole instanceof NodeList) {
+            NodeList nl = (NodeList) oRole;
+            for (int i = 0; i < nl.getLength(); ++i) {
+                roles.put(nl.item(i).getTextContent());
+            }
+        }
+        return roles;
     }
 
     private JSONObject getXmlInfos(File f) throws Exception {
