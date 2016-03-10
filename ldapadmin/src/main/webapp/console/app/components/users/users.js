bf20c43cbdb6555c9d0e9f810d00c7e74a448e83
Antoine Abt <antoine@abt.im>
2016-03-10 14:49:38 +0100

    Move users controller to es6 class

--- ldapadmin/src/main/webapp/console/app/components/users/users.js
+++ ldapadmin/src/main/webapp/console/app/components/users/users.js
@@ -1,27 +1,31 @@
-angular.module('admin_console')
-.controller('UsersController', [
-  '$routeParams', 'User', 'Group',
-  UsersController
-]);
+class UsersController {
 
-function UsersController($routeParams, User, Group) {
-  this.users = User.query(function() {
-    this.allUsers = this.users.slice();
-  }.bind(this));
+  constructor($routeParams, User, Group) {
 
-  this.groups = Group.query(function(){
-    this.activeGroup = this.groups.filter(function(g) {
-      return g.cn == $routeParams.id }
-    )[0];
-    if (this.activeGroup) {
-      this.filter(this.activeGroup);
-    }
-  }.bind(this));
+    this.users = User.query(function() {
+      this.allUsers = this.users.slice()
+    }.bind(this))
+
+    this.groups = Group.query(function(){
+      this.activeGroup = this.groups.filter(function(g) {
+        return g.cn == $routeParams.id }
+      )[0];
+      if (this.activeGroup) {
+        this.filter(this.activeGroup)
+      }
+    }.bind(this))
+
+  }
+
+  filter(group) {
+    if (!this.allUsers) { return; }
+    this.users = this.allUsers.filter(function(user) {
+      return group.users.indexOf(user.uid) >= 0;
+    })
+  }
+
 }
 
-UsersController.prototype.filter = function(group) {
-  if (!this.allUsers) { return; }
-  this.users = this.allUsers.filter(function(user) {
-    return group.users.indexOf(user.uid) >= 0;
-  });
-};
+UsersController.$inject = [ '$routeParams', 'User', 'Group' ]
+
+angular.module('admin_console').controller('UsersController', UsersController)
