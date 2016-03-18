56e2b7513a9d1894a65d69083183790202fbb81c
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2016-03-18 11:46:47 +0100

    coding style + enhanced readability

--- mapfishapp/src/main/webapp/app/addons/measurements/js/main.js
+++ mapfishapp/src/main/webapp/app/addons/measurements/js/main.js
@@ -2,8 +2,14 @@ Ext.namespace("GEOR.Addons");
 
 GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
 
-    window: null,
     toggleGroup: 'measurements',
+
+    window: null,
+    layer: null,
+    lengthAction: null,
+    areaAction: null,
+    resetAction: null,
+    exportAction: null,
 
     /**
      * Method: init
@@ -24,6 +30,7 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
                 })
             )
         });
+
         this.lengthAction =  new GeoExt.Action({
             control: new OpenLayers.Control.DynamicMeasure(OpenLayers.Handler.Path, {
                 accuracy: this.options.accuracy,
@@ -45,6 +52,7 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
             text: OpenLayers.i18n("measurements.distance"),
             iconAlign: 'top'
         });
+
         this.areaAction =  new GeoExt.Action({
             control: new OpenLayers.Control.DynamicMeasure(OpenLayers.Handler.Polygon, {
                 accuracy: this.options.accuracy,
@@ -66,18 +74,10 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
             text: OpenLayers.i18n("measurements.area"),
             iconAlign: 'top'
         });
-        this.measuresReset = new Ext.Action({
-            handler: function () {
-                if (this.layer) {
-                    this.layer.removeAllFeatures();
-                }
-                var removeAllFeaturesCallback = function () {
-                    this.removeAllFeatures();
-                }
-                this._loopOnMatchingLayers(this.map, this.dynamicMesurePattern,
-                    removeAllFeaturesCallback);
-                this.measuresReset.items[0].toggle();
-            },
+
+        this.resetAction = new Ext.Action({
+            handler: this.resetHandler,
+            scope: this,
             map: this.map,
             toggleGroup: this.toggleGroup,
             allowDepress: true,
@@ -86,92 +86,20 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
             tooltip: this.tr("measurements.reset.tooltip"),
             iconCls: 'measurements-delete',
             text: OpenLayers.i18n("measurements.reset"),
-            iconAlign: 'top',
-            scope: this
+            iconAlign: 'top'
         });
-        this.exportAsKml = new Ext.Action({
-            //code from: src/main/webapp/app/addons/annotation/js/Annotation.js
-            handler: function() {
-                GEOR.waiter.show();
-                var format = new OpenLayers.Format.KML({
-                        'extractAttributes': true,
-                        'foldersName': OpenLayers.i18n("measurements.tools"),
-                        'internalProjection': this.map.getProjectionObject(),
-                        'externalProjection': new OpenLayers.Projection("EPSG:4326")
-                    });
-                var kmlFeatures = this.layer.features;
-                for (i = 0; i < kmlFeatures.length; i++) {
-                    var geometry = kmlFeatures[i].geometry;
-                    var aLength = this.lengthAction.control.getBestLength(geometry);
-                    sLength = this.formatMeasure(aLength);
-                    kmlFeatures[i].data.length_measure = sLength[0];
-                    kmlFeatures[i].data.length_units = sLength[1];
 
-
-                    if (geometry instanceof OpenLayers.Geometry.Polygon) {
-                        var area = this.areaAction.control.getBestArea(geometry);
-                        sArea = this.formatMeasure(area);
-                        kmlFeatures[i].data.area_measure = sArea[0];
-                        kmlFeatures[i].data.area_units = sArea[1];
-                        var label = kmlFeatures[i].data.area_measure + ' ' +
-                                kmlFeatures[i].data.area_units + '² (' +
-                                kmlFeatures[i].data.length_measure + ' ' +
-                                kmlFeatures[i].data.length_units + ')';
-                    } else {
-                        var label = kmlFeatures[i].data.length_measure + ' ' +
-                                    kmlFeatures[i].data.length_units;
-                    }
-
-                    kmlFeatures[i].attributes.name = label;
-                    kmlFeatures[i].attributes.description = label;
-               }
-               var setNameDescriptionKmlCallback = function(argsObj) {
-                   var feature = null, measure = null,
-                       areaPattern = /(^OpenLayers.Control.DynamicMeasure)(.)*(AreaKeep$)/
-                   for (j = 0; j < this.features.length; j++) {
-                        feature = this.features[j];
-                        //Square unit for area
-                        if (areaPattern.test(this.name)) {
-                            measure = feature.data.measure + ' ' +
-                                feature.data.units + '²';
-                        } else {
-                            measure = feature.data.measure + ' ' +
-                                feature.data.units;
-                        }
-                        feature.attributes.name = measure;
-                        feature.attributes.description = measure;
-                    }
-                    argsObj.kmlFeatures = argsObj.kmlFeatures.concat(this.features);
-               }
-               var kmlObj = {kmlFeatures: kmlFeatures};
-               this._loopOnMatchingLayers(this.map, this.dynamicMesurePattern,
-                    setNameDescriptionKmlCallback, kmlObj);
-
-                var olKML = format.write(kmlObj.kmlFeatures),
-                    kmlStyle = "<Style id='measureFeatureStyle'><LineStyle><width>2</width><color>ff6666636</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle><LabelStyle><color>ff170580</color></LabelStyle><IconStyle><color>00ffffff</color><Icon><href>http:/maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href></Icon></IconStyle></Style>",
-                    styleInHeadKML = olKML.replace(/<Folder>/g, '<Folder>' + kmlStyle),
-                    styleInHeadAndPlacemarksKML = styleInHeadKML.replace(/<Placemark><name>/g,'<Placemark><styleUrl>#measureFeatureStyle</styleUrl><name>');
-                OpenLayers.Request.POST({
-                    url: GEOR.config.PATHNAME + "/ws/kml/",
-                    data: styleInHeadAndPlacemarksKML,
-                    success: function(response) {
-                        var o = Ext.decode(response.responseText);
-                        window.location.href = GEOR.config.PATHNAME + "/" + o.filepath;
-                    }
-                });
-                this.exportAsKml.items[0].toggle();
-            },
+        this.exportAction = new Ext.Action({
+            handler: this.exportHandler,
+            scope: this,
             map: this.map,
-            toggleGroup: this.toggleGroup,
-            allowDepress: true,
-            pressed: false,
             minWidth: 50,
             tooltip: this.tr("measurements.export.tooltip"),
             iconCls: 'measurements-export',
             text: OpenLayers.i18n("measurements.export"),
-            iconAlign: 'top',
-            scope: this
+            iconAlign: 'top'
         });
+
         this.window = new Ext.Window({
             title: OpenLayers.i18n('measurements.tools'),
             width: 240,
@@ -183,40 +111,20 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
             items: [{
                 xtype: 'toolbar',
                 border: false,
-                items: [this.lengthAction, this.areaAction, this.measuresReset,
-                            this.exportAsKml]
+                items: [
+                    this.lengthAction,
+                    this.areaAction,
+                    this.resetAction,
+                    this.exportAction
+                ]
             }],
             listeners: {
-                'show': function() {
-                    if (OpenLayers.Util.indexOf(this.map.layers, this.layer) < 0) {
-                        this.map.addLayer(this.layer);
-                    }
-                    // Show annotation layers
-                    var setVisibilityCallback = function() {
-                        this.setVisibility(true);
-                    }
-                    this._loopOnMatchingLayers(this.map, this.dynamicMesurePattern,
-                        setVisibilityCallback);
-                },
-                'hide': function() {
-                    // Hide draw layer
-                    if (OpenLayers.Util.indexOf(this.map.layers, this.layer) > 0) {
-                        this.map.removeLayer(this.layer);
-                    }
-                    // Hide annotation layers
-                    var setVisibilityCallback = function() {
-                        this.setVisibility(false);
-                    }
-                    this._loopOnMatchingLayers(this.map, this.dynamicMesurePattern,
-                        setVisibilityCallback);
-                    this.areaAction.control.deactivate();
-                    this.lengthAction.control.deactivate();
-                    //Deactivate measurements tool in menus
-                    this.item.setChecked(false);
-                },
+                'show': this.onShow,
+                'hide': this.onHide,
                 scope: this
             }
         });
+
         if (this.target) {
             // create a button to be inserted in toolbar:
             this.components = this.target.insertButton(this.position, {
@@ -242,6 +150,136 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
         }
     },
 
+
+    /**
+     * Method: onShow
+     *
+     */
+    onShow: function() {
+        if (OpenLayers.Util.indexOf(this.map.layers, this.layer) < 0) {
+            this.map.addLayer(this.layer);
+        }
+        // Show annotation layers
+        this.loopOnMeasureLayers(function(layer) {
+            layer.setVisibility(true);
+        });
+    },
+
+
+    /**
+     * Method: onHide
+     *
+     */
+    onHide: function() {
+        // Hide draw layer
+        if (OpenLayers.Util.indexOf(this.map.layers, this.layer) > 0) {
+            this.map.removeLayer(this.layer);
+        }
+        // Hide annotation layers
+        this.loopOnMeasureLayers(function(layer) {
+            layer.setVisibility(false);
+        });
+        this.areaAction.control.deactivate();
+        this.lengthAction.control.deactivate();
+        // Deactivate measurements tool in menus
+        this.item.setChecked(false);
+    },
+
+
+    /**
+     * Method: resetHandler
+     *
+     */
+    resetHandler: function() {
+        if (this.layer) {
+            this.layer.removeAllFeatures();
+        }
+        this.loopOnMeasureLayers(function(layer) {
+            layer.removeAllFeatures();
+        });
+        this.resetAction.items[0].toggle();
+    },
+
+
+    /**
+     * Method: exportHandler
+     *
+     */
+    exportHandler: function() {
+        // code from: src/main/webapp/app/addons/annotation/js/Annotation.js
+        GEOR.waiter.show();
+        var format = new OpenLayers.Format.KML({
+            'extractAttributes': true,
+            'foldersName': OpenLayers.i18n("measurements.tools"),
+            'internalProjection': this.map.getProjectionObject(),
+            'externalProjection': new OpenLayers.Projection("EPSG:4326")
+        });
+
+        Ext.each(this.layer.features, function(feature) {
+            var label = '', geometry = feature.geometry,
+                data = feature.data,
+                length = this.formatMeasure(
+                    this.lengthAction.control.getBestLength(geometry)
+                );
+
+            data.length_measure = length[0];
+            data.length_units = length[1];
+
+            if (geometry instanceof OpenLayers.Geometry.Polygon) {
+                var area = this.formatMeasure(
+                    this.areaAction.control.getBestArea(geometry)
+                );
+                data.area_measure = area[0];
+                data.area_units = area[1];
+
+                label = [data.area_measure, ' ',
+                    data.area_units, '² (', 
+                    data.length_measure, ' ', 
+                    data.length_units, ')'
+                ].join('');
+
+            } else {
+                label = data.length_measure + ' ' + data.length_units;
+            }
+
+            feature.attributes.name = label;
+            feature.attributes.description = label;
+        }, this);
+
+        var kmlFeatures = [];
+        this.loopOnMeasureLayers(function(layer) {
+            var areaPattern = /(^OpenLayers.Control.DynamicMeasure)(.)*(AreaKeep$)/;
+
+            Ext.each(layer.features, function(feature) {
+                var measure,
+                    data = feature.data;
+                // Square unit for area
+                if (areaPattern.test(this.name)) {
+                    measure = data.measure + ' ' + data.units + '²';
+                } else {
+                    measure = data.measure + ' ' + data.units;
+                }
+                feature.attributes.name = measure;
+                feature.attributes.description = measure;
+            }, this);
+
+            kmlFeatures = kmlFeatures.concat(layer.features);
+        });
+
+        OpenLayers.Request.POST({
+            url: GEOR.config.PATHNAME + "/ws/kml/",
+            data: format.write(kmlFeatures)
+                .replace(/<Folder>/g, '<Folder>' + this.options.KMLStyle)
+                .replace(/<Placemark><name>/g,'<Placemark><styleUrl>#measureFeatureStyle</styleUrl><name>'),
+            success: function(response) {
+                var o = Ext.decode(response.responseText);
+                window.location.href = GEOR.config.PATHNAME + "/" + o.filepath;
+            }
+        });
+
+    },
+
+
     /**
      * Method: tr
      *
@@ -249,6 +287,7 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
     tr: function(a) {
         return OpenLayers.i18n(a);
     },
+
 
     /**
      * Method: _onCheckchange
@@ -268,23 +307,26 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
         }
     },
 
+
     /**
-     * Method: _loopOnMatchingLayers
+     * Method: loopOnMeasureLayers
      *
      * Private use.
-     * fun is a function that will be called with a matching layer and
-     *     the argsObj as parameter
+     * fun is a function that will be called with a matching layer
      */
-    _loopOnMatchingLayers: function (map, pattern, fun, argsObj) {
-        argsObj = argsObj || null;
-        var layerName = null;
-        for (i = 0; i < map.layers.length; i++) {
-            layerName = map.layers[i].name
-            if (pattern.test(layerName)) {
-                fun.call(map.layers[i], argsObj);
+    loopOnMeasureLayers: function(fun) {
+        // Regular expression associated with layer.name 
+        // which contains measurement annotations 
+        // managed by OpenLayers.Control.DynamicMeasure
+        var pattern = /(^OpenLayers.Control.DynamicMeasure)(.)*(Keep$)/;
+
+        Ext.each(this.map.layers, function(layer) {
+            if (pattern.test(layer.name)) {
+                fun.call(this, layer);
             }
-        }
+        }, this);
     },
+
 
     /**
      * Method : formatMeasure
@@ -300,19 +342,13 @@ GEOR.Addons.Measurements = Ext.extend(GEOR.Addons.Base, {
      *
      */
     formatMeasure: function(measure) {
+        // FIXME: NUMBER !
         measure[0] = OpenLayers.Number.format(
             Number(measure[0].toPrecision(this.options.accuracy)), null);
 
         return measure;
     },
 
-    /**
-     * Property: dynamicMesurePattern
-     *
-     * Regular expression associated with layer.name which contains measurement
-     * annotations managed by OpenLayers.Control.DynamicMeasure
-     */
-    dynamicMesurePattern: /(^OpenLayers.Control.DynamicMeasure)(.)*(Keep$)/,
 
     /**
      * Method: destroy
