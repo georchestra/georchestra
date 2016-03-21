c1527bfe1c359a9f2df9f14aa3b55b8ce8adcc83
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-03-21 16:44:47 +0100

    Listen on unix socket
    Required for backup via slapcat

--- ldap/docker/start.sh
+++ ldap/docker/start.sh
@@ -3,7 +3,7 @@ set -e
 
 /populate_ldap.sh &
 sleep 1
-exec /entrypoint.sh slapd -d 32768 -u openldap -g openldap
+exec /entrypoint.sh slapd -d 32768 -u openldap -g openldap -h ldap:/// ldapi:///
 
 
 
