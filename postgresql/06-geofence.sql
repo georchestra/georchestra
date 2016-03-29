45925e06497cee1adf6044a94c551634b645e375
Julien Acroute <julien.acroute@camptocamp.com>
2016-03-29 15:51:43 +0200

    Add 'gf_adminrule' table and fix case of SQL keywords

--- postgresql/06-geofence.sql
+++ postgresql/06-geofence.sql
@@ -1,10 +1,10 @@
-begin;
+BEGIN;
 
-create schema geofence;
+CREATE SCHEMA geofence;
 
-set search_path to geofence;
+SET search_path TO geofence;
 
-create table gf_gfuser (
+CREATE TABLE gf_gfuser (
   id int8 not null,
   dateCreation timestamp,
   emailAddress varchar(255),
@@ -13,10 +13,10 @@ create table gf_gfuser (
   fullName varchar(255),
   name varchar(255) not null unique,
   password varchar(255),
-  primary key (id)
+  PRIMARY KEY (id)
 );
 
-create table gf_gsinstance (
+CREATE TABLE gf_gsinstance (
   id int8 not null,
   baseURL varchar(255) not null,
   dateCreation timestamp,
@@ -24,39 +24,39 @@ create table gf_gsinstance (
   name varchar(255) not null,
   password varchar(255) not null,
   username varchar(255) not null,
-  primary key (id)
+  PRIMARY KEY (id)
 );
 
-create table gf_gsuser (
+CREATE TABLE gf_gsuser (
   id int8 not null,
   admin bool not null,
   dateCreation timestamp,
   emailAddress varchar(255),
   enabled bool not null,
-  extId varchar(255) unique,
+  extId varchar(255) UNIQUE,
   fullName varchar(255),
-  name varchar(255) not null unique,
+  name varchar(255) not null UNIQUE,
   password varchar(255),
-  primary key (id)
+  PRIMARY KEY (id)
 );
 
-create table gf_layer_attributes (
+CREATE TABLE gf_layer_attributes (
   details_id int8 not null,
   access_type varchar(255),
   data_type varchar(255),
   name varchar(255) not null,
-  primary key (details_id, name),
-  unique (details_id, name)
+  PRIMARY KEY (details_id, name),
+  UNIQUE (details_id, name)
 );
 
-create table gf_layer_custom_props (
+CREATE TABLE gf_layer_custom_props (
   details_id int8 not null,
   propvalue varchar(255),
   propkey varchar(255),
-  primary key (details_id, propkey)
+  PRIMARY KEY (details_id, propkey)
 );
 
-create table gf_layer_details (
+CREATE TABLE gf_layer_details (
   id int8 not null,
   area public.geometry,
   cqlFilterRead varchar(4000),
@@ -66,16 +66,16 @@ create table gf_layer_details (
   type varchar(255),
   rule_id int8 not null,
   catalog_mode character varying(255),
-  primary key (id),
-  unique (rule_id)
+  PRIMARY KEY (id),
+  UNIQUE (rule_id)
 );
 
-create table gf_layer_styles (
+CREATE TABLE gf_layer_styles (
   details_id int8 not null,
   styleName varchar(255)
 );
 
-create table gf_rule (
+CREATE TABLE gf_rule (
   id int8 not null,
   grant_type varchar(255) not null,
   layer varchar(255),
@@ -91,98 +91,117 @@ create table gf_rule (
   ip_size integer,
   rolename character varying(255),
   username character varying(255),
-  primary key (id),
-  unique (gsuser_id, userGroup_id, instance_id, service, request, workspace, layer)
+  PRIMARY KEY (id),
+  UNIQUE (gsuser_id, userGroup_id, instance_id, service, request, workspace, layer)
 );
 
-create table gf_rule_limits (
+CREATE TABLE gf_rule_limits (
   id int8 not null,
   area public.geometry,
   rule_id int8 not null,
   catalog_mode character varying(255),
-  primary key (id),
-  unique (rule_id)
+  PRIMARY KEY (id),
+  UNIQUE (rule_id)
 );
 
-create table gf_user_usergroups (
+CREATE TABLE gf_user_usergroups (
   user_id int8 not null,
   group_id int8 not null,
-  primary key (user_id, group_id)
+  PRIMARY KEY (user_id, group_id)
 );
 
-create table gf_usergroup (
+CREATE TABLE gf_usergroup (
   id int8 not null,
   dateCreation timestamp,
   enabled bool not null,
-  extId varchar(255) unique,
-  name varchar(255) not null unique,
-  primary key (id),
-  CONSTRAINT gf_usergroup_extid_key UNIQUE (extid),
-  CONSTRAINT gf_usergroup_name_key UNIQUE (name)
+  extId varchar(255) UNIQUE,
+  name varchar(255) not null UNIQUE,
+  PRIMARY KEY (id)
 );
 
-create index idx_gsuser_name on gf_gsuser (name);
+CREATE TABLE gf_adminrule (
+  id bigserial NOT NULL,
+  grant_type character varying(255) NOT NULL,
+  ip_high bigint,
+  ip_low bigint,
+  ip_size integer,
+  priority bigint NOT NULL,
+  rolename character varying(255),
+  username character varying(255),
+  workspace character varying(255),
+  instance_id bigint,
+  CONSTRAINT gf_adminrule_pkey PRIMARY KEY (id),
+  CONSTRAINT gf_adminrule_username_rolename_instance_id_workspace_key UNIQUE (username, rolename, instance_id, workspace)
+);
 
-alter table gf_layer_attributes
-add constraint fk_attribute_layer
-foreign key (details_id)
-references gf_layer_details;
+CREATE index idx_gsuser_name on gf_gsuser (name);
 
-alter table gf_layer_custom_props
-add constraint fk_custom_layer
-foreign key (details_id)
-references gf_layer_details;
+ALTER TABLE gf_layer_attributes
+ADD CONSTRAINT fk_attribute_layer
+FOREIGN KEY (details_id)
+REFERENCES gf_layer_details;
 
-alter table gf_layer_details
-add constraint fk_details_rule
-foreign key (rule_id)
-references gf_rule;
+ALTER TABLE gf_layer_custom_props
+ADD CONSTRAINT fk_custom_layer
+FOREIGN KEY (details_id)
+REFERENCES gf_layer_details;
 
-alter table gf_layer_styles
-add constraint fk_styles_layer
-foreign key (details_id)
-references gf_layer_details;
+ALTER TABLE gf_layer_details
+ADD CONSTRAINT fk_details_rule
+FOREIGN KEY (rule_id)
+REFERENCES gf_rule;
 
-create index idx_rule_request on gf_rule (request);
+ALTER TABLE gf_layer_styles
+ADD CONSTRAINT fk_styles_layer
+FOREIGN KEY (details_id)
+REFERENCES gf_layer_details;
 
-create index idx_rule_layer on gf_rule (layer);
+ALTER TABLE gf_adminrule
+ADD CONSTRAINT fk_adminrule_instance FOREIGN KEY (instance_id)
+REFERENCES geofence.gf_gsinstance (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-create index idx_rule_service on gf_rule (service);
+CREATE index idx_rule_request on gf_rule (request);
 
-create index idx_rule_workspace on gf_rule (workspace);
+CREATE index idx_rule_layer on gf_rule (layer);
 
-create index idx_rule_priority on gf_rule (priority);
+CREATE index idx_rule_service on gf_rule (service);
 
-alter table gf_rule
-add constraint fk_rule_user
-foreign key (gsuser_id)
-references gf_gsuser;
+CREATE index idx_rule_workspace on gf_rule (workspace);
 
-alter table gf_rule
-add constraint fk_rule_usergroup
-foreign key (userGroup_id)
-references gf_usergroup;
+CREATE index idx_rule_priority on gf_rule (priority);
 
-alter table gf_rule
-add constraint fk_rule_instance
-foreign key (instance_id)
-references gf_gsinstance;
+ALTER TABLE gf_rule
+ADD CONSTRAINT fk_rule_user
+FOREIGN KEY (gsuser_id)
+REFERENCES gf_gsuser;
 
-alter table gf_rule_limits
-add constraint fk_limits_rule
-foreign key (rule_id)
-references gf_rule;
+ALTER TABLE gf_rule
+ADD CONSTRAINT fk_rule_usergroup
+FOREIGN KEY (userGroup_id)
+REFERENCES gf_usergroup;
 
-alter table gf_user_usergroups
-add constraint fk_uug_user
-foreign key (user_id)
-references gf_gsuser;
+ALTER TABLE gf_rule
+ADD CONSTRAINT fk_rule_instance
+FOREIGN KEY (instance_id)
+REFERENCES gf_gsinstance;
 
-alter table gf_user_usergroups
-add constraint fk_uug_group
-foreign key (group_id)
-references gf_usergroup;
-create sequence hibernate_sequence;
+ALTER TABLE gf_rule_limits
+ADD CONSTRAINT fk_limits_rule
+FOREIGN KEY (rule_id)
+REFERENCES gf_rule;
 
-commit;
+ALTER TABLE gf_user_usergroups
+ADD CONSTRAINT fk_uug_user
+FOREIGN KEY (user_id)
+REFERENCES gf_gsuser;
+
+ALTER TABLE gf_user_usergroups
+ADD CONSTRAINT fk_uug_group
+FOREIGN KEY (group_id)
+REFERENCES gf_usergroup;
+
+CREATE sequence hibernate_sequence;
+
+COMMIT;
 
