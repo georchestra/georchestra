947d36aed5f684b589d2992b58256ffbfb9171d4
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2016-05-31 10:42:13 +0200

    Atlas - wip
    - Fixing dependency hell with spring and mfprint / camel, requiring both
      a different incompatible spring version
    - Adding missing deps at runtime for mfprint (see issue
      https://github.com/mapfish/mapfish-print/issues/418)
    - WIP on the camel component actually responsible of calling mfprint.

--- atlas/src/main/java/org/georchestra/atlas/CamelMapfishPrintComponent.java
+++ atlas/src/main/java/org/georchestra/atlas/CamelMapfishPrintComponent.java
@@ -3,8 +3,11 @@ package org.georchestra.atlas;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Handler;
@@ -14,8 +17,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.mapfish.print.MapPrinter;
+import org.mapfish.print.cli.Main;
+import org.mapfish.print.wrapper.json.PJsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -25,24 +33,29 @@ import com.itextpdf.text.pdf.PdfWriter;
 public class CamelMapfishPrintComponent {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
-    private final MapPrinter mp = new MapPrinter();
+    private MapPrinter mapPrinter = null;
+    private ApplicationContext context = null;
+
+    public void init() throws IOException, URISyntaxException {
+        this.context = new ClassPathXmlApplicationContext(Main.DEFAULT_SPRING_CONTEXT);
+        this.mapPrinter = context.getBean(MapPrinter.class);
+        URL configFileUrl = this.getClass().getResource("/atlas/config.yaml");
+        Assert.notNull(configFileUrl);
+        byte[] configFileData = FileUtils.readFileToByteArray(new File(configFileUrl.toURI()));
+        this.mapPrinter.setConfiguration(configFileUrl.toURI(), configFileData);
+    }
+
+    
     @Handler
     public void toMapfishPrintPdf(Exchange ex) throws JSONException, DocumentException, URISyntaxException, IOException {
         String rawJson = ex.getProperty("rawJson", String.class);
-        JSONObject jobSpec = new JSONObject(new JSONTokener(rawJson));
-        URI configFile = this.getClass().getResource("/atlas/config.yaml").toURI();
-        //byte[] configFileData = FileUtils.readFileToByteArray(new File(configFile));
-        //mp.setConfiguration(configFile, configFileData);
 
-        // To be continued
+        Assert.notNull(this.mapPrinter);
+
         try {
-            Document document = new Document();
             ByteArrayOutputStream baos =  new ByteArrayOutputStream();
-            PdfWriter.getInstance(document, baos);
-            document.open();
-            document.add(new Paragraph(jobSpec.toString(4)));
-            document.close();
-
+            PJsonObject mfSpec = this.mapPrinter.parseSpec(rawJson); 
+            this.mapPrinter.print(mfSpec, baos);
             Message m = ex.getIn();
             m.setBody(baos.toByteArray());
             ex.setOut(m);
