12cd0ad6c96297c783bec82b702b44468481fe6b
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2016-09-28 16:28:34 +0200

    atlas - datadirizing mapfish-print v3 configuration
    See related PR on the datadir repository
    (https://github.com/georchestra/datadir/pull/36)

--- atlas/src/main/java/org/georchestra/atlas/CamelMapfishPrintComponent.java
+++ atlas/src/main/java/org/georchestra/atlas/CamelMapfishPrintComponent.java
@@ -1,14 +1,16 @@
 package org.georchestra.atlas;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.commons.io.FileUtils;
+import org.georchestra.commons.configuration.GeorchestraConfiguration;
 import org.json.JSONException;
 import org.json.JSONWriter;
 import org.mapfish.print.MapPrinter;
@@ -16,30 +18,37 @@ import org.mapfish.print.cli.Main;
 import org.mapfish.print.wrapper.json.PJsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.Assert;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.net.URISyntaxException;
-import java.net.URL;
+import com.google.common.annotations.VisibleForTesting;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class CamelMapfishPrintComponent {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private MapPrinter mapPrinter = null;
 
+    @Autowired
+    private GeorchestraConfiguration georConfiguration ;
+
     public void init() throws IOException, URISyntaxException {
         ApplicationContext context = new ClassPathXmlApplicationContext(Main.DEFAULT_SPRING_CONTEXT);
         this.mapPrinter = context.getBean(MapPrinter.class);
 
-        URL configFileUrl = this.getClass().getResource("/atlas/config.yaml");
+        URI configFileUrl = this.getClass().getResource("/atlas/config.yaml").toURI();
+        if (this.georConfiguration != null && this.georConfiguration.activated()) {
+            configFileUrl = new File(this.georConfiguration.getContextDataDir() +
+                    "/mapfishprintv3/config.yaml").toURI();
+        }
         Assert.notNull(configFileUrl);
-        byte[] configFileData = FileUtils.readFileToByteArray(new File(configFileUrl.toURI()));
-        this.mapPrinter.setConfiguration(configFileUrl.toURI(), configFileData);
+        byte[] configFileData = FileUtils.readFileToByteArray(new File(configFileUrl));
+        this.mapPrinter.setConfiguration(configFileUrl, configFileData);
     }
 
     public void toMapfishPrintPdf(Exchange ex)
