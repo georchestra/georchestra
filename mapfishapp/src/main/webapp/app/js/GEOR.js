c1913f2ae78f86d493ec73a3201376c4228cbf8f
Julien SABATIER <julien.sabatier@lepuyenvelay.fr>
2017-07-06 09:37:05 +0200

    Enable text selection in gridviews

--- mapfishapp/src/main/webapp/app/js/GEOR.js
+++ mapfishapp/src/main/webapp/app/js/GEOR.js
@@ -91,6 +91,16 @@ Ext.namespace("GEOR");
             return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
         };
     }
+    
+    // Redefine the grid template to enable text selection
+    if (!Ext.grid.GridView.prototype.templates) {
+        Ext.grid.GridView.prototype.templates = {};
+    }
+    Ext.grid.GridView.prototype.templates.cell = new Ext.Template(
+        '<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} x-selectable {css}" style="{style}" tabIndex="0" {cellAttr}>',
+        '<div class="x-grid3-cell-inner x-grid3-col-{id}" {attr}>{value}</div>',
+        '</td>'
+    );
 })();
 
 
