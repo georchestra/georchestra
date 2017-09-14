1c324fef035c623539fc320b2351e33ae2b7b843
Julien Acroute <julien.acroute@camptocamp.com>
2017-09-14 09:16:49 +0200

    security-proxy - Remove unused import

--- security-proxy/src/main/java/org/georchestra/security/LdapUserDetailsRequestHeaderProvider.java
+++ security-proxy/src/main/java/org/georchestra/security/LdapUserDetailsRequestHeaderProvider.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Matcher;
