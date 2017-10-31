8ebf4031acc4ae1521b827f302308f8ca47b01fb
Julien Acroute <julien.acroute@camptocamp.com>
2017-10-31 11:34:12 +0100

    Add index on ogc stats tables
    This commit modify trigger in order to automaticly add indexes on stats
    tables. It will create index when 'rotating' logs: when a new table is
    created, indexes will be added to previous table.
    This commit also remove patch of postgres entrypoint. Instead, a script
    was added to clear *-data.sql file if IGNORE_DATA=yes.

--- postgresql/06-ogc-server-statistics.sql
+++ postgresql/06-ogc-server-statistics.sql
@@ -16,7 +16,7 @@ CREATE TABLE ogc_services_log(
   id bigserial,
   request character varying(20),
   org character varying(255),
-  roles text[]	
+  roles text[]
 );
 
 CREATE OR REPLACE FUNCTION get_partition_table(my_date timestamp without time zone)
@@ -26,6 +26,10 @@ DECLARE
     my_table_name character varying;
     my_month character varying;
     my_year character varying;
+    previous_month character varying;
+    previous_year character varying;
+    previous_table_name character varying;
+    previous_table_oid oid;
     borne_sup date;
     borne_inf date;
     query character varying;
@@ -33,33 +37,76 @@ DECLARE
     base_schema_name character varying = 'ogcstatistics';
 BEGIN
 
-	-- Generate table name
-	my_month := EXTRACT(MONTH FROM my_date);
-	my_year := EXTRACT(YEAR FROM my_date);
-	
-	my_table_name := base_table_name || '_y' || my_year || 'm' || my_month;
+  -- Generate table name
+  my_month := EXTRACT(MONTH FROM my_date);
+  my_year := EXTRACT(YEAR FROM my_date);
 
-	-- RAISE NOTICE 'table name %.%', base_schema_name, my_table_name;
+  my_table_name := base_table_name || '_y' || my_year || 'm' || my_month;
 
-	-- Test if table already exists
-	IF NOT (SELECT count(*) > 0 
-	        FROM information_schema.tables 
-	        WHERE table_schema = base_schema_name
-		AND table_name = my_table_name) THEN
+  -- RAISE NOTICE 'table name %.%', base_schema_name, my_table_name;
 
-		borne_inf := (my_year || '-' || my_month || '-01')::date;
-		borne_sup := borne_inf + INTERVAL '1 month';
+  -- Test if table already exists
+  IF NOT (SELECT count(*) > 0
+          FROM information_schema.tables
+          WHERE table_schema = base_schema_name
+    AND table_name = my_table_name) THEN
 
-		query := 'CREATE TABLE ' || base_schema_name || '.' || my_table_name || '( CHECK ( date >= DATE ''' || borne_inf || ''' AND date < DATE ''' || borne_sup || ''' ) ';
-		query := query || ') INHERITS (' || base_schema_name || '.' || base_table_name || ')';
+    borne_inf := (my_year || '-' || my_month || '-01')::date;
+    borne_sup := borne_inf + INTERVAL '1 month';
 
-		-- Create table if it does not exists
-		EXECUTE query;
+    query := 'CREATE TABLE ' || base_schema_name || '.' || my_table_name || '( CHECK ( date >= DATE ''' || borne_inf || ''' AND date < DATE ''' || borne_sup || ''' ) ';
+    query := query || ') INHERITS (' || base_schema_name || '.' || base_table_name || ')';
 
-	END IF;
+    -- Create table if it does not exists
+    EXECUTE query;
 
-	RETURN base_schema_name || '.' || my_table_name;
-	
+    -- Create Indexes on previous table for user_name and date fields
+    previous_month := EXTRACT(MONTH FROM (my_date - INTERVAL '1 month'));
+    previous_year := EXTRACT(YEAR FROM (my_date - INTERVAL '1 month'));
+    previous_table_name := base_table_name || '_y' || previous_year || 'm' || previous_month;
+
+    -- Check if previous table exists
+    IF (SELECT count(*) > 0
+        FROM information_schema.tables
+        WHERE table_schema = base_schema_name
+        AND table_name = previous_table_name) THEN
+
+      previous_table_oid := (base_schema_name || '.' || previous_table_name)::regclass::int;
+      -- Check if indexes already exists
+      IF NOT (WITH stat_indexes AS (SELECT t.oid,
+                                           t.relname AS table_name,
+                                           i.relname AS index_name,
+                                           array_agg(a.attname) AS column_names
+                                    FROM
+                                       pg_class t,
+                                       pg_class i,
+                                       pg_index ix,
+                                       pg_attribute a
+                                    WHERE
+                                       t.oid = ix.indrelid
+                                       AND i.oid = ix.indexrelid
+                                       AND a.attrelid = t.oid
+                                       AND a.attnum = ANY(ix.indkey)
+                                       AND t.relkind = 'r'
+                                       AND t.oid = previous_table_oid
+                                    GROUP BY t.oid, t.relname, index_name)
+              SELECT count(*) = 2
+              FROM stat_indexes
+              WHERE column_names IN (ARRAY['date']::name[], ARRAY['user_name']::name[])) THEN
+
+        query := 'CREATE INDEX ' || previous_table_name || '_date_idx ON ' || base_schema_name || '.' || previous_table_name || '(date)';
+        EXECUTE query;
+        query := 'CREATE INDEX ' || previous_table_name || '_user_name_idx ON ' || base_schema_name || '.' || previous_table_name || '(user_name)';
+        EXECUTE query;
+
+      END IF;
+
+    END IF;
+
+  END IF;
+
+  RETURN base_schema_name || '.' || my_table_name;
+
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE;
@@ -71,16 +118,15 @@ COMMENT ON FUNCTION get_partition_table(timestamp without time zone) IS 'Table n
 CREATE OR REPLACE FUNCTION insert_stat_trigger_function()
 RETURNS TRIGGER AS $$
 DECLARE
-	table_name character varying;
+  table_name character varying;
 BEGIN
 
-	table_name := ogcstatistics.get_partition_table(NEW.date);
+  table_name := ogcstatistics.get_partition_table(NEW.date);
 
-	-- insert record in child table
-	EXECUTE 'INSERT INTO ' || table_name || ' VALUES ($1.*)' USING NEW;
-
-	-- do *not* insert record in master table
-	RETURN NULL;
+  -- insert record in child table
+  EXECUTE 'INSERT INTO ' || table_name || ' VALUES ($1.*)' USING NEW;
+  -- do *not* insert record in master table
+  RETURN NULL;
 
 END;
 $$
