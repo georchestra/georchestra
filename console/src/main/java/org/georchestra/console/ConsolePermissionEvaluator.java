637b6e4a35f1d6317fa778e771c1e91458f5d214
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2018-04-06 11:52:04 +0200

    console - first shot on spring-security filtering
    Pair prog @vampouille

--- console/src/main/java/org/georchestra/console/ConsolePermissionEvaluator.java
+++ console/src/main/java/org/georchestra/console/ConsolePermissionEvaluator.java
@@ -1,31 +1,76 @@
 package org.georchestra.console;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.georchestra.console.ws.backoffice.roles.RolesController;
+import org.georchestra.console.dao.DelegationDao;
+import org.georchestra.console.ds.OrgsDao;
+import org.georchestra.console.dto.Org;
+import org.georchestra.console.dto.Role;
+import org.georchestra.console.model.DelegationEntry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class ConsolePermissionEvaluator implements PermissionEvaluator {
 
-	private static final Log LOG = LogFactory.getLog(RolesController.class.getName());
+    private static final Log LOG = LogFactory.getLog(ConsolePermissionEvaluator.class.getName());
+    private static GrantedAuthority ROLE_SUPERADMIN = new SimpleGrantedAuthority("ROLE_ADMINISTRATOR");
 
-	public ConsolePermissionEvaluator() {
-		LOG.info("consolePermissionEvaluator bean initialized");
-	}
-	@Override
-	public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
-		LOG.info("ConsolePermissionEvaluator: Here");
-		return false;
-	}
+    @Autowired
+    private DelegationDao delegationDao;
 
-	@Override
-	public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType,
-			Object permission) {
-		LOG.info("ConsolePermissionEvaluator: Here");
-		return false;
-	}
+    @Autowired
+    private OrgsDao orgsDao;
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        if (isSuperAdministrator(authentication)) {
+            return true;
+        } else {
+            // TODO: refactor with Delegation2 class
+            String username = authentication.getName();
+            DelegationEntry delegation = delegationDao.findOne(username);
+            if (delegation == null) {
+                return false;
+            }
+            String[] orgs = delegation.getOrgs();
+            HashSet<String> delegationMembers = new HashSet<String>();
+            for (String o : orgs) {
+                Org orga = orgsDao.findByCommonName(o);
+                delegationMembers.addAll(orga.getMembers());
+            }
+            // TODO: this should be done before, because no need
+            // to iterate before if not of type Role
+            if (targetDomainObject instanceof Role) {
+                Role r = (Role) targetDomainObject;
+                List<String> userList = r.getUserList();
+                userList.retainAll(delegationMembers);
+                return Arrays.asList(delegation.getRoles()).contains(r.getName());
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType,
+            Object permission) {
+        if (isSuperAdministrator(authentication)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSuperAdministrator(Authentication authentication) {
+        return authentication.getAuthorities().contains(ROLE_SUPERADMIN);
+    }
 
 }
