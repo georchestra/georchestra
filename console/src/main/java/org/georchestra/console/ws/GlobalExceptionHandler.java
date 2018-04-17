24c70c5966be8ca0dd58a9ab45bd55337d03e898
Julien Acroute <julien.acroute@camptocamp.com>
2018-04-17 17:13:45 +0200

    Console - Improve error management

--- console/src/main/java/org/georchestra/console/ws/GlobalExceptionHandler.java
+++ console/src/main/java/org/georchestra/console/ws/GlobalExceptionHandler.java
@@ -10,34 +10,43 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Log LOG = LogFactory.getLog(GlobalExceptionHandler.class.getName());
 
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(value = HttpStatus.FORBIDDEN)
-    public void handleAccessDenied(Exception e, HttpServletResponse response) throws Exception {
-        LOG.error(e.getMessage());
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        throw e;
-    }
-
-    @ExceptionHandler(NameNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public void handleNotFound(Exception e, HttpServletResponse response) throws Exception {
-        LOG.error(e.getMessage());
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        throw e;
-    }
-
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleException(Exception e, HttpServletResponse response) throws Exception {
-        LOG.error(e.getMessage());
-        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        throw e;
+    public void handleException(Exception e, HttpServletResponse response){
+
+        // Set HTTP response code according to exception type
+        if(e instanceof NameNotFoundException){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } else if(e instanceof AccessDeniedException){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+        // Generate stack trace as String
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw, true);
+        e.printStackTrace(pw);
+
+        // Log error and stack trace
+        LOG.error(sw.getBuffer().toString());
+
+        // Send error message to browser
+        PrintWriter output = null;
+        try {
+            output = response.getWriter();
+            output.write(e.toString());
+            output.close();
+        } catch (IOException ex) {}
+
     }
 
 }
