6dd9b2547c36aa69764bfd2466386a0a07dad540
Gabriel Roldan <gabriel.roldan@gmail.com>
2019-01-22 01:48:04 -0300

    Use the mapped docker database port in ConsoleIntegrationTest
    Move instatiation of dataSource bean to ConsoleIntegrationTest
    to set up the postgres connection URL using the mapped port
    from the georchestraDatabase docker container.

--- console/src/test/java/org/georchestra/console/ws/utils/ConsoleIntegrationTest.java
+++ console/src/test/java/org/georchestra/console/ws/utils/ConsoleIntegrationTest.java
@@ -1,16 +1,28 @@
 package org.georchestra.console.ws.utils;
 
-import org.junit.AfterClass;
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+
+import org.georchestra.commons.WaitForDb;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.containers.GenericContainer;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /**
  * This class defines test against a real LDAP server, from the official
@@ -20,42 +32,82 @@ import org.testcontainers.containers.GenericContainer;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/webmvc-config-test.xml"})
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ContextConfiguration(locations = { "classpath:/webmvc-config-test.xml" })
 public class ConsoleIntegrationTest {
 
 	@ClassRule
 	public static GenericContainer georchestraLdap = new GenericContainer("georchestra/ldap")
-		.withExposedPorts(389)
-		.withEnv("SLAPD_ORGANISATION", "georchestra")
-		.withEnv("SLAPD_DOMAIN", "georchestra.org")
-		.withEnv("SLAPD_PASSWORD", "secret")
-		.withEnv("SLAPD_ADDITIONAL_MODULES", "groupofmembers");
+			.withExposedPorts(389)
+			.withEnv("SLAPD_ORGANISATION", "georchestra")
+			.withEnv("SLAPD_DOMAIN", "georchestra.org")
+			.withEnv("SLAPD_PASSWORD", "secret")
+			.withEnv("SLAPD_ADDITIONAL_MODULES", "groupofmembers");
 
 	@ClassRule
 	public static GenericContainer georchestraDatabase = new GenericContainer("georchestra/database")
-		.withExposedPorts(5432)
-		.withEnv("POSTGRES_USER", "georchestra")
-		.withEnv("POSTGRES_PASSWORD", "georchestra");
+			.withExposedPorts(5432)
+			.withEnv("POSTGRES_USER", "georchestra")
+			.withEnv("POSTGRES_PASSWORD", "georchestra");
 
-	
-	@Autowired
-	private LdapTemplate ldapTemplate;
 
-	@BeforeClass
-	public static void setUp() {
-		georchestraLdap.start();
-		georchestraDatabase.start();
+	// <bean id="waitForDb" class="org.georchestra.commons.WaitForDb"
+	// init-method="test">
+	// <property name="url" value="${psql.url}"/>
+	// <property name="username" value="${psql.user}"/>
+	// <property name="password" value="${psql.pass}"/>
+	// <property name="driverClassName" value="org.postgresql.Driver"/>
+	// </bean>
+	public @Bean(name = "waitForDb") @Lazy(false) WaitForDb tcpIpPropertySource() {
+		Integer dbport = georchestraDatabase.getMappedPort(5432);
+		String psqlurl = String.format("jdbc:postgresql://localhost:%d/georchestra", dbport.intValue());
+
+		WaitForDb bean = new WaitForDb();
+		bean.setDriverClassName("org.postgresql.Driver");
+		bean.setUsername("georchestra");
+		bean.setPassword("georchestra");
+		bean.setUrl(psqlurl);
+		bean.test();
+		return bean;
 	}
-	
-	@AfterClass
-	public static void tearDown() {
-		georchestraLdap.stop();
-		georchestraDatabase.stop();
+
+	// <bean id="dataSource" class="com.mchange.v2.c3p0.ComboPooledDataSource"
+	// depends-on="waitForDb">
+	// <property name="jdbcUrl" value="${psql.url}"/>
+	// <property name="user" value="${psql.user}"/>
+	// <property name="password" value="${psql.pass}"/>
+	// <property name="driverClass" value="org.postgresql.Driver"/>
+	// <property name="maxPoolSize" value="10"/>
+	// <property name="minPoolSize" value="1"/>
+	// <property name="automaticTestTable" value="cpds_connection_test"/>
+	// </bean>
+	public @Bean(name = "dataSource") @DependsOn("waitForDb") @Autowired DataSource psqlDataSource(WaitForDb wd)
+			throws PropertyVetoException {
+
+		ComboPooledDataSource pool = new ComboPooledDataSource();
+		pool.setJdbcUrl(wd.getUrl());
+		pool.setUser(wd.getUsername());
+		pool.setPassword(wd.getPassword());
+		pool.setDriverClass(wd.getDriverClassName());
+		pool.setMaxPoolSize(10);
+		pool.setMinPoolSize(1);
+		pool.setAutomaticTestTable("cpds_connection_test");
+		return pool;
 	}
-	
+
+	private @Autowired LdapTemplate ldapTemplate;
+
+	public @BeforeClass static void setUp() {
+		System.out.println("pg mapped port: " + georchestraDatabase.getMappedPort(5432));
+	}
+
+	public @Before void before() {
+	}
+
 	@Test
 	public void testTheTest() throws InterruptedException {
 		System.out.println(ldapTemplate.lookup("cn=admin"));
 	}
-	
+
 }
