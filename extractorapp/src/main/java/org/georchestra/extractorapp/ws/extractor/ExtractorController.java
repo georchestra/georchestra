georchestra/georchestra

commit 156e342b0260d3ea79bc9de059027a2a247d071a

Use Spring placeholders to access datadir properties

extractorapp/src/main/java/org/georchestra/extractorapp/ws/extractor/ExtractorController.java
@@ -43,7 +43,6 @@ import javax.sql.DataSource;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.georchestra.commons.configuration.GeorchestraConfiguration;
 import org.georchestra.extractorapp.ws.AbstractEmailFactory;
 import org.georchestra.extractorapp.ws.Email;
 import org.georchestra.extractorapp.ws.extractor.task.ExecutionMetadata;
@@ -93,24 +92,9 @@ public class ExtractorController implements ServletContextAware {
     private ExtractionManager extractionManager;
     private String userAgent;
 
-    @Autowired
-    private GeorchestraConfiguration georConfig;
     private @Autowired DataSource dataSource;
 
     public void validateConfig() throws PropertyVetoException, MalformedURLException {
-        if ((georConfig != null) && (georConfig.activated())) {
-            LOG.info("geOrchestra datadir: reconfiguring bean ...");
-            this.setPublicUrl(georConfig.getProperty("publicUrl"));
-            maxCoverageExtractionSize = Long.parseLong(georConfig.getProperty("maxCoverageExtractionSize"));
-            remoteReproject = Boolean.parseBoolean(georConfig.getProperty("remoteReproject"));
-            useCommandLineGDAL = Boolean.parseBoolean(georConfig.getProperty("useCommandLineGDAL"));
-            extractionFolderPrefix = georConfig.getProperty("extractionFolderPrefix");
-            String username = georConfig.getProperty("privileged_admin_name");
-            String password = georConfig.getProperty("privileged_admin_pass");
-            // Recreating a Credentials object
-            adminCredentials = new UsernamePasswordCredentials(username, password);
-            LOG.info("geOrchestra datadir: done.");
-        }
         if (extractionManager == null) {
             throw new AssertionError("A extractionManager needs to be defined in spring configuration");
         }

extractorapp/src/main/webapp/WEB-INF/ws-servlet.xml
@@ -40,18 +40,19 @@
     </bean>
 
     <bean id="extractor-controller" init-method="validateConfig" class="org.georchestra.extractorapp.ws.extractor.ExtractorController">
+        <property name="publicUrl" value="${publicUrl:https://georchestra.mydomain.org}"/>
         <property name="responseTemplateFile" value="/WEB-INF/templates/extractor-http-response-template.xml"/>
         <property name="emailFactory" ref="emailFactory"/>
         <property name="reponseMimeType" value="application/xml"/>
         <property name="responseCharset" value="UTF-8"/>
-        <property name="maxCoverageExtractionSize" value="99999999"/>
-        <property name="remoteReproject" value="true"/>
-        <property name="useCommandLineGDAL" value="false"/>
-        <property name="extractionFolderPrefix" value="proj-extract-"/>
+        <property name="maxCoverageExtractionSize" value="${maxCoverageExtractionSize:99999999}"/>
+        <property name="remoteReproject" value="${remoteReproject:true}"/>
+        <property name="useCommandLineGDAL" value="${useCommandLineGDAL:false}"/>
+        <property name="extractionFolderPrefix" value="${extractionFolderPrefix:extraction-}"/>
         <property name="adminCredentials">
             <bean class="org.apache.commons.httpclient.UsernamePasswordCredentials">
-                <constructor-arg index="0" value="geoserver_privileged_user"/>
-                <constructor-arg index="1" value="xxxxxxxx"/>
+                <constructor-arg index="0" value="${privileged_admin_name:geoserver_privileged_user}"/>
+                <constructor-arg index="1" value="${privileged_admin_pass:gerlsSnFd6SmM}"/>
             </bean>
           </property>
           <property name="extractionManager" ref="extractionManager"/>

commit 6de49678b6a7983fd67f918abc343d44108fd8dc

Fix exception when setting publicUrl property
Bug introduced by 156e342b0260d3ea79bc9de059027a2a247d071a commit.
Note: here we rename the setPublicUrl method since in reality it sets the
secureHost and the servletUrl properties.

extractorapp/src/main/java/org/georchestra/extractorapp/ws/extractor/ExtractorController.java
@@ -81,6 +81,7 @@ public class ExtractorController implements ServletContextAware {
     private AbstractEmailFactory emailFactory;
     private ServletContext servletContext;
     private String servletUrl;
+    private String publicUrl;
     private String extractionFolderPrefix;
     private boolean remoteReproject = true;
     private boolean useCommandLineGDAL = false;
@@ -95,6 +96,7 @@ public class ExtractorController implements ServletContextAware {
     private @Autowired DataSource dataSource;
 
     public void validateConfig() throws PropertyVetoException, MalformedURLException {
+        setSecureHostAndServletUrl(this.publicUrl);
         if (extractionManager == null) {
             throw new AssertionError("A extractionManager needs to be defined in spring configuration");
         }
@@ -360,8 +362,11 @@ public class ExtractorController implements ServletContextAware {
      * Each instance of {link} will be replaced with the extraction bundle URL
      */
 
-    public void setPublicUrl(String rawPublicUrl) throws MalformedURLException {
+    public void setPublicUrl(String publicUrl) {
+        this.publicUrl = publicUrl;
+    }
 
+    public void setSecureHostAndServletUrl(String rawPublicUrl) throws MalformedURLException {
         // extract hostname to set set secureHost
         URL publicURL = new URL(rawPublicUrl);
         this.secureHost = publicURL.getHost();