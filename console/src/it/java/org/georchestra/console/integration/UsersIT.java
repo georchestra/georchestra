1a48186ff8bfb8c386ec3502de2d892f6ecc4bc5
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2019-02-28 11:28:13 +0100

    refatcor test

--- console/src/it/java/org/georchestra/console/integration/UsersIT.java
+++ console/src/it/java/org/georchestra/console/integration/UsersIT.java
@@ -2,33 +2,22 @@ package org.georchestra.console.integration;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.georchestra.console.integration.instruments.WithMockRandomUidUser;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -42,33 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class UsersIT {
 
-    private @Autowired
-    LdapTemplate ldapTemplateSanityCheck;
-
-    private @Value("${ldap_port}") int ldapPort;
-
-    private @Value("${psql_port}") int psqlPort;
-
-    public @Rule
-    TestName testName = new TestName();
-
-    @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
-
-    public static @BeforeClass
-    void init() {
-    }
-
-    public @Before
-    void before() {
-        System.err.printf("############# %s: psql_port: %s, ldap_port: %s\n", testName.getMethodName(), psqlPort, ldapPort);
-        // pre-flight sanity check
-        assertNotNull(ldapTemplateSanityCheck.lookup("cn=admin"));
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-
-    }
+    public @Rule @Autowired IntegrationTestSupport support;
 
     @WithMockRandomUidUser
     public @Test
@@ -95,12 +58,12 @@ public class UsersIT {
         String newUserName = ("IT_USER_" + RandomStringUtils.randomAlphabetic(8)).toLowerCase();
         createUser(userName);
 
-        mockMvc.perform(put("/private/users/" + userName).content(readRessourceToString("/testData/createUserPayload.json")
+        support.perform(put("/private/users/" + userName).content(readRessourceToString("/testData/createUserPayload.json")
                 .replace("{uuid}", newUserName)
                 .replace("psc", "cra")));
 
 
-        mockMvc.perform(get("/private/users/" + newUserName))
+        support.perform(get("/private/users/" + newUserName))
                 .andExpect(jsonPath("$.org").value("cra"))
                 .andExpect(jsonPath("$.uid").value(newUserName));
     }
@@ -111,7 +74,7 @@ public class UsersIT {
         String userName = ((User)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
         createUser(userName);
 
-        mockMvc.perform(get("/account/userdetails").header("sec-username", userName)).andExpect(status().isOk());
+        support.perform(get("/account/userdetails").header("sec-username", userName)).andExpect(status().isOk());
     }
 
     @WithMockRandomUidUser
@@ -120,7 +83,7 @@ public class UsersIT {
         String userName = ((User)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
         createUser(userName);
 
-        mockMvc.perform(get("/private/users"))
+        support.perform(get("/private/users"))
                 .andExpect(jsonPath("$[?(@.pending in [false])].['pending']").exists())
                 .andExpect(jsonPath("$[?(@.pending in [true])].['pending']").exists());
     }
@@ -140,34 +103,34 @@ public class UsersIT {
         setRole(userName1, role3Name, role4Name);
 
         String body = String.format("{ \"users\":[\"%s\",\"%s\"],\"PUT\":[\"%s\",\"%s\"],\"DELETE\":[\"%s\",\"%s\"]}", userName1, userName2, role1Name, role3Name, role2Name, role4Name);
-        this.mockMvc.perform(post("/private/roles_users").content(body));
+        support.perform(post("/private/roles_users").content(body));
 
-        mockMvc.perform(get("/private/roles/" + role1Name)).andExpect(jsonPath("$.users[0]").value(userName1));
-        mockMvc.perform(get("/private/roles/" + role1Name)).andExpect(jsonPath("$.users[1]").value(userName2));
-        mockMvc.perform(get("/private/roles/" + role3Name)).andExpect(jsonPath("$.users[0]").value(userName1));
-        mockMvc.perform(get("/private/roles/" + role3Name)).andExpect(jsonPath("$.users[1]").value(userName2));
-        mockMvc.perform(get("/private/roles/" + role2Name)).andExpect(jsonPath("$.users").isEmpty());
-        mockMvc.perform(get("/private/roles/" + role4Name)).andExpect(jsonPath("$.users").isEmpty());
+        support.perform(get("/private/roles/" + role1Name)).andExpect(jsonPath("$.users[0]").value(userName1));
+        support.perform(get("/private/roles/" + role1Name)).andExpect(jsonPath("$.users[1]").value(userName2));
+        support.perform(get("/private/roles/" + role3Name)).andExpect(jsonPath("$.users[0]").value(userName1));
+        support.perform(get("/private/roles/" + role3Name)).andExpect(jsonPath("$.users[1]").value(userName2));
+        support.perform(get("/private/roles/" + role2Name)).andExpect(jsonPath("$.users").isEmpty());
+        support.perform(get("/private/roles/" + role4Name)).andExpect(jsonPath("$.users").isEmpty());
     }
 
     private void createUser(String userName) throws Exception {
-        mockMvc.perform(post("/private/users").content(readRessourceToString("/testData/createUserPayload.json").replace("{uuid}", userName)));
+        support.perform(post("/private/users").content(readRessourceToString("/testData/createUserPayload.json").replace("{uuid}", userName)));
     }
 
     private ResultActions getProfile() throws Exception {
-        return this.mockMvc.perform(get("/private/users/profile"));
+        return support.perform(get("/private/users/profile"));
     }
 
     private String createRole() throws Exception {
         String roleName = "IT_ROLE_" + RandomStringUtils.randomAlphabetic(8).toUpperCase();
         String body = "{ \"cn\": \"" + roleName + "\", \"description\": \"Role Description\", \"isFavorite\": false }";
-        this.mockMvc.perform(post("/private/roles").content(body));
+        support.perform(post("/private/roles").content(body));
         return roleName;
     }
 
     private void setRole(String userName, String role1Name, String role2Name) throws Exception {
         String body = "{ \"users\":[\"" + userName + "\"],\"PUT\":[\""+ role1Name + "\", \"" + role2Name + "\"],\"DELETE\":[]}";
-        this.mockMvc.perform(post("/private/roles_users").content(body));
+        support.perform(post("/private/roles_users").content(body));
     }
 
     private String readRessourceToString(String name) throws URISyntaxException, IOException {
