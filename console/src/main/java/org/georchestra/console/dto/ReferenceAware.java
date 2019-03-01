765c0263115c93440cfbf676b33bd297096f4669
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2019-03-01 17:21:08 +0100

    refactor (planning to ad a third facet on org)

--- console/src/main/java/org/georchestra/console/dto/ReferenceAware.java
+++ console/src/main/java/org/georchestra/console/dto/ReferenceAware.java
@@ -1,8 +1,17 @@
 package org.georchestra.console.dto;
 
+import org.georchestra.console.ds.OrgsDao;
 import org.springframework.ldap.core.DirContextAdapter;
 
-public interface ReferenceAware {
+public interface ReferenceAware<T> {
     DirContextAdapter getReference();
-   void setReference(DirContextAdapter reference);
+
+    void setReference(DirContextAdapter reference);
+
+    void setPending(boolean pending);
+
+    OrgsDao.Extension<T> getExtension(OrgsDao orgDao);
+
+    boolean isPending();
+
 }
