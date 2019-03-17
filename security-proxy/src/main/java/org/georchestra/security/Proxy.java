4f6d4aa0da5f0a4f57934973e3a754d04cffeb74
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2019-03-17 23:50:09 +0100

    so to allow complex request handling (post with file upload for example)

--- security-proxy/src/main/java/org/georchestra/security/Proxy.java
+++ security-proxy/src/main/java/org/georchestra/security/Proxy.java
@@ -786,7 +786,7 @@ public class Proxy {
 
         HttpAsyncRequestProducer producer = new BasicAsyncRequestProducer(
                 new HttpHost(proxyingRequest.getURI().getHost(), proxyingRequest.getURI().getPort()),
-                new BasicHttpRequest(proxyingRequest.getRequestLine()));
+                proxyingRequest);
 
         httpclient.execute(producer, consumer, null);
 
