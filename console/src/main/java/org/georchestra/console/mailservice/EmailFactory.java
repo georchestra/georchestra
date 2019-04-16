e26e5fd97bf8cb2fa2ea0403ebe21caca7b57020
Landry Breuil <breuil@craig.fr>
2019-04-16 11:06:29 +0200

    fix typo in comment

--- console/src/main/java/org/georchestra/console/mailservice/EmailFactory.java
+++ console/src/main/java/org/georchestra/console/mailservice/EmailFactory.java
@@ -93,7 +93,7 @@ public class EmailFactory {
 	}
 
 	/**
-	 * e-mail to the user to inform the account requires the moderator's singnup
+	 * e-mail to the user to inform the account requires the moderator's validation
 	 */
 	public void sendAccountCreationInProcessEmail(ServletContext servletContext, String recipient,
 												  String userName, String uid) throws MessagingException {
