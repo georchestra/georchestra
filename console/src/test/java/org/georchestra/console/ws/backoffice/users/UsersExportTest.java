c9f221a618c1634af3af6482b4be87ccb8474337
Rémi Desgrange <remi.desgrange@camptocamp.com>
2019-08-01 13:33:16 +0200

    Fix test

--- console/src/test/java/org/georchestra/console/ws/backoffice/users/UsersExportTest.java
+++ console/src/test/java/org/georchestra/console/ws/backoffice/users/UsersExportTest.java
@@ -50,9 +50,24 @@ public class UsersExportTest {
     @Test
     public void testGetUsersAsCsv() throws Exception {
         String s = us.getUsersAsCsv("[\"pmauduit\"]");
+
+        String headers = "First Name,Middle Name,Last Name,Title,Suffix,Initials,Web Page,Gender,Birthday,Anniversary,"
+                + "Location,Language,Internet Free Busy,Notes,E-mail Address,E-mail 2 Address,E-mail 3 Address,Primary Phone,Home Phone,"
+                + "Home Phone 2,Mobile Phone,Pager,Home Fax,Home Address,Home Street,Home Street 2,Home Street 3,Home Address PO Box,Home City,"
+                + "Home State,Home Postal Code,Home Country,Spouse,Children,Manager's Name,Assistant's Name,Referred By,Company Main Phone,"
+                + "Business Phone,Business Phone 2,Business Fax,Assistant's Phone,Company,Job Title,Department,Office Location,Organizational ID Number,"
+                + "Profession,Account,Business Address,Business Street,Business Street 2,Business Street 3,Business Address PO Box,Business City,"
+                + "Business State,Business Postal Code,Business Country,Other Phone,Other Fax,Other Address,Other Street,Other Street 2,Other Street 3,"
+                + "Other Address PO Box,Other City,Other State,Other Postal Code,Other Country,Callback,Car Phone,ISDN,Radio Phone,TTY/TDD Phone,Telex,"
+                + "User 1,User 2,User 3,User 4,Keywords,Mileage,Hobby,Billing Information,Directory Server,Sensitivity,Priority,Private,Categories";
+        String res = "Pierre,,Mauduit,,,,,,,,,,,,abc@example.com,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," +
+                ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,";
+        String[] splitted = s.split("\r\n");
+
         assertFalse("The CSV contains \"null\", unexpected", s.contains("null"));
         assertTrue("The CSV should contain \"abc@example.com\"", s.contains("abc@example.com"));
-        assertTrue("The CSV should have the headers", s.startsWith("pom"));
+        assertEquals("The CSV should have the headers", splitted[0], headers);
+        assertEquals("The CSV payoad should match entry data", splitted[1], res);
     }
 
     @Test
