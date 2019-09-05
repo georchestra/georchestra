4e516d900b4b70cf4893ef90329b4e64a317994d
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2019-09-05 12:49:03 +0200

    integration tests - Using @Sql annotation instead of loading fixtures by hand

--- analytics/src/test/java/org/georchestra/analytics/StatisticsControllerIT.java
+++ analytics/src/test/java/org/georchestra/analytics/StatisticsControllerIT.java
@@ -1,33 +1,27 @@
 package org.georchestra.analytics;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
+@Sql(scripts = "fixtures.sql", config = @SqlConfig(dataSource = "jpaDataSource"))
 @ContextConfiguration(locations = { "classpath:/ws-servlet.xml" })
 public class StatisticsControllerIT {
 
@@ -35,26 +29,7 @@ public class StatisticsControllerIT {
 
     private @Value("${dlJdbcUrlOGC}") String dlJdbcUrlOGC;
 
-    private @Autowired BasicDataSource ds;
-
     protected final Log logger = LogFactory.getLog(getClass().getPackage().getName());
-
-    private static boolean dataLoaded = false;
-
-    public @Before void before() throws Exception {
-        if (!dataLoaded) {
-            URL res = StatisticsControllerIT.class.getResource("fixtures.sql");
-            assertNotNull(res);
-            String fixtures = FileUtils.readFileToString(new File(res.toURI()));
-            Connection c = ds.getConnection();
-            for (String stmt : fixtures.split("\n")) {
-                logger.info(stmt);
-                PreparedStatement ps = c.prepareStatement(stmt);
-                ps.execute();
-            }
-            dataLoaded = true;
-        }
-    }
 
     public @Test void testFilteringByOrg() throws Exception {
         String payload = "{\"service\":\"layersUsage.json\",\"startDate\":\"2019-07-23\","
