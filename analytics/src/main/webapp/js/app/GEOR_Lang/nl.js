40b554f5e6895e21d8cc390d16d3fbd4c091a217
bendeg <benoitdeg@gmail.com>
2020-01-17 14:48:46 +0100

    nl added, 7 addons left : atlas, bangeocoder, cadastre, magnifier, notes, openls, template

---
+++ analytics/src/main/webapp/js/app/GEOR_Lang/nl.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) Camptocamp
+ *
+ * This file is part of geOrchestra
+ *
+ * geOrchestra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with geOrchestra.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Dutch translation file
+ */
+Analytics.Lang.nl = {
+    "Loading...": "Bezig met laden...",
+    "Page": "Pagina",
+    "of N1": "op {0}",
+    "Name": "Naam",
+    "Layer": "Laag",
+    "Layers": "Lagen",
+    "File": "Bestand",
+    "Files": "Bestanden",
+    "Users": "Gebruikers",
+    "Organism": "Organisatie",
+    "Organisms": "Organisaties",
+    "Request": "Query",
+    "Metadata": "Metadata",
+    "from service": " van service ",
+    "OGC Service": "OGC Service",
+    "OGC Services": "OGC Services",
+    "Service URL": "Service URL",
+    "next month": "volgende maand",
+    "previous month": "vorige maand",
+    "Number of requests": "Aantal queries",
+    "Global statistics": "Algemene statistieken",
+    "Monthly statistics": "Maandelijkse statistieken",
+    "Personalized extractions": "Gepersonaliseerde extracties",
+    "Downloads from GeoNetwork": "Downloads van GeoNetwork",
+    "Select this tab to access the statistics of downloads from the catalog": "Selecteer dit tabblad voor toegang tot de statistieken van downloads uit de catalogus",   
+    "Select this tab to access to the statistics of the extractor": "Selecteer dit tabblad voor toegang tot de statistieken van de extractor",
+    "Select this tab to access the OGC service statistics": "Selecteer dit tabblad voor toegang tot de statistieken van de OGC-services van het platform",
+    "double-click to see details": "dubbelklik om details te bekijken",
+    "The layers have been downloaded by the user": "Lagen gedownload door de gebruiker",
+    "Latest users who downloaded the OWSTYPE layer": "Gebruikers die de ${OWSTYPE}-laag hebben gedownload",
+    "Files downloaded by the user": "Bestanden gedownload door de gebruiker",
+    "The last users who downloaded the file": "Gebruikers die het bestand hebben gedownload",
+    "from metadata": "uit de metadata",
+    "Records N0 to N1 of N2": "Records {0} tot {1} op {2}",
+    "No records": "Geen records",
+    "Export to CSV": "CSV Export"    
+    // no trailing comma
+};
