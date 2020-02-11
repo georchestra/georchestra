adcf9623434b0a87b19920de9c5f15f937b6b9bf
Rémi Desgrange <remi.desgrange@camptocamp.com>
2020-02-11 11:44:12 +0100

    Fix absolute path in bash jsbuild script (#2936)
    * Fix absolute path in bash jsbuild script
    
    * (Try) fixing CI
    
    * fix (again) CI
    
    * make the jsbuild install more robust
    
    * Trying to fix shell script problems
    
    * Adapting to a more recent virtualenv
    
    * trigger the CI
    
    * restoring submodules
    
    * trigger build
    
    Co-authored-by: François Van Der Biest <francois.vanderbiest@camptocamp.com>

--- mapfishapp/jsbuild/build.sh
+++ mapfishapp/jsbuild/build.sh
@@ -9,12 +9,12 @@ venv="${buildpath}/env"
 #
 # Command path definitions
 #
-python="/usr/bin/python2"
-virtualenv="/usr/bin/virtualenv --python=${python}"
-mkdir="/bin/mkdir"
-rm="/bin/rm"
-sh="/bin/sh"
-cp="/bin/cp"
+python="/usr/bin/env python2"
+virtualenv="/usr/bin/env virtualenv --python=${python}"
+mkdir="/usr/bin/env mkdir"
+rm="/usr/bin/env rm"
+sh="/usr/bin/env sh"
+cp="/usr/bin/env cp"
 
 #
 # build
@@ -26,12 +26,22 @@ fi
 ${mkdir} -p ${releasepath} ${releasepath}/lang
 
 (cd ${buildpath};
+ ${python} -c "import pip"
+ if [ $? != 0 ]; then
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    ${python} get-pip.py
+    rm get-pip.py
+ fi
+ ${python} -c "import virtualenv"
+ if [ $? != 0 ]; then
+   ${python} -m pip install virtualenv
+ fi
  ${venv}/bin/jsbuild -h > /dev/null
  if  [ ! -d ${venv} ] || [ $? -eq 0 ]; then
      echo "creating virtual env and installing jstools..."
      rm -rf ${venv}
-     ${virtualenv} --no-site-packages ${venv}
-     ${venv}/bin/pip install jstools==0.6 -i https://pypi.python.org/simple/
+     ${python} -m virtualenv ${venv}
+     ${venv}/bin/python -m pip install jstools==0.6 -i https://pypi.python.org/simple/
      echo "done."
  fi;
 
