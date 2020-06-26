cee4f5a8011b2926f39cb5eb09856f92c311d941
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2020-06-26 14:25:04 +0200

    mfapp - improve logging
    * Avoid calls to printStackTrace() use logger with error level instead
    * Better error feedback in case connecting to WFS ends up with an error
      because of bad credentials (401)
    
    Tests:
    
    * testsuite OK

--- mapfishapp/src/main/java/org/georchestra/mapfishapp/ws/OGCProxy.java
+++ mapfishapp/src/main/java/org/georchestra/mapfishapp/ws/OGCProxy.java
@@ -40,6 +40,8 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -68,6 +70,7 @@ public class OGCProxy {
      */
     private String[] _allowedHosts = {};
 
+    private static final Log LOG = LogFactory.getLog(OGCProxy.class.getPackage().getName());
     /**
      * List of valid content types
      */
@@ -332,7 +335,7 @@ public class OGCProxy {
             streamToClient.close();
         } catch (IOException e) {
             // connection problem with the host
-            e.printStackTrace();
+            LOG.error(String.format("I/O exception occured while proxyfying to %s", sURL), e);
         }
     }
 
