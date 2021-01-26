594ab185a888d2db8b6a2fd64fca673c76eb51af
Björn Höfling <bjoern.hoefling@camptocamp.com>
2021-01-26 18:56:17 +0100

    Add Controller, Service and Tasklets for Publishing.

--- datafeeder/src/main/java/org/georchestra/datafeeder/api/DataPublishingApiController.java
+++ datafeeder/src/main/java/org/georchestra/datafeeder/api/DataPublishingApiController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 by the geOrchestra PSC
+ * Copyright (C) 2020, 2021 by the geOrchestra PSC
  *
  * This file is part of geOrchestra.
  *
@@ -18,22 +18,36 @@
  */
 package org.georchestra.datafeeder.api;
 
-import java.util.Optional;
+import java.util.UUID;
 
+import javax.annotation.security.RolesAllowed;
+
+import org.georchestra.datafeeder.model.DataUploadJob;
+import org.georchestra.datafeeder.service.DataPublishingService;
+import org.georchestra.datafeeder.service.DataUploadValidityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.annotations.Api;
 
 @Controller
+@RolesAllowed({ "ROLE_USER", "ROLE_ADMINISTRATOR" })
 @Api(tags = { "Data Publishing" }) // hides the empty data-publishing-api-controller entry in swagger-ui.html
 public class DataPublishingApiController implements DataPublishingApi {
 
-    private @Autowired NativeWebRequest currentRequest;
+    private @Autowired DataUploadValidityService validityService;
+    private @Autowired DataPublishingService dataPublishingService;
 
-    public @Override Optional<NativeWebRequest> getRequest() {
-        return Optional.of(currentRequest);
+    public ResponseEntity<PublishJobStatus> publish(@PathVariable("jobId") UUID jobId,
+            @RequestBody(required = false) PublishRequest publishRequest) {
+        DataUploadJob uploadPacket = validityService.getAndCheckAccessRights(jobId);
+        dataPublishingService.publish(uploadPacket);
+        //TODO: Response from the service
+        return ResponseEntity.ok(null);
+
     }
 
 }
