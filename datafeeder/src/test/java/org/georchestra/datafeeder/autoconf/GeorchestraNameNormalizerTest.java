34f00b04f63398e1713284ef9863e9d13cdc4c4d
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-02-23 17:53:46 -0300

    chore: format, organize imports, add missinc (c) headers

--- datafeeder/src/test/java/org/georchestra/datafeeder/autoconf/GeorchestraNameNormalizerTest.java
+++ datafeeder/src/test/java/org/georchestra/datafeeder/autoconf/GeorchestraNameNormalizerTest.java
@@ -1,16 +1,27 @@
+/*
+ * Copyright (C) 2020 by the geOrchestra PSC
+ *
+ * This file is part of geOrchestra.
+ *
+ * geOrchestra is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * geOrchestra is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * geOrchestra.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.georchestra.datafeeder.autoconf;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.UUID;
 
-import org.georchestra.datafeeder.autoconf.GeorchestraNameNormalizer;
 import org.junit.Before;
 import org.junit.Test;
 
