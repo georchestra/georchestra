d028814a3c4ec90b47dd93b86877f2705d941b3d
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-02-24 19:01:22 -0300

    Upgrade openapi-generator and switch to springfox-boot-starter v3
    Use io.springfox:springfox-boot-starter:3.0.0 to automatically generate
    OAS3 swagger documentation. The old springfox-swagger2:2.9.2 results in
    dependency hell when enabling hateoas.
    
    This new version is also recommended for spring-boot 2.

--- datafeeder/src/main/java/org/georchestra/datafeeder/swagger/SwaggerDocConfig.java
+++ datafeeder/src/main/java/org/georchestra/datafeeder/swagger/SwaggerDocConfig.java
@@ -18,36 +18,18 @@
  */
 package org.georchestra.datafeeder.swagger;
 
-import org.openapitools.configuration.OpenAPIDocumentationConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SuppressWarnings("deprecation")
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
-@Import(OpenAPIDocumentationConfig.class)
-public class SwaggerDocConfig extends WebMvcConfigurerAdapter {
+@EnableSwagger2
+public class SwaggerDocConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/api/swagger-ui.html");
-        registry.addRedirectViewController("/api", "/api/swagger-ui.html");
-        registry.addRedirectViewController("/api/", "/api/swagger-ui.html");
-
-        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
-        registry.addRedirectViewController("/api/swagger-resources/configuration/ui",
-                "/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/api/swagger-resources/configuration/security",
-                "/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/api/swagger-ui.html**")
-                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-        registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addRedirectViewController("/", "/swagger-ui/index.html");
     }
 }
