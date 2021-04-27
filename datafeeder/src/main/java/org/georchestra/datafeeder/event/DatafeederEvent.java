96506c2bd1bda2b95c08d4ec97808cefabcd3643
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-04-27 11:10:31 -0300

    Initial datafeeder application events model

---
+++ datafeeder/src/main/java/org/georchestra/datafeeder/event/DatafeederEvent.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 by the geOrchestra PSC
+ *
+ * This file is part of geOrchestra.
+ *
+ * geOrchestra is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * geOrchestra is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * geOrchestra.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.georchestra.datafeeder.event;
+
+import org.georchestra.datafeeder.model.UserInfo;
+import org.springframework.context.ApplicationEvent;
+
+import lombok.Getter;
+
+public class DatafeederEvent<T> extends ApplicationEvent {
+
+    private static final long serialVersionUID = 1L;
+    private @Getter UserInfo user;
+
+    public DatafeederEvent(T source, UserInfo user) {
+        super(source);
+        this.user = user;
+    }
+
+    @SuppressWarnings("unchecked")
+    public @Override T getSource() {
+        return (T) super.getSource();
+    }
+}
