f0e7b695d5d85241085122e49b78b9ce2612ee65
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2021-08-17 16:39:50 +0200

    proxy - webdav - adding tests

--- security-proxy/src/test/java/org/georchestra/security/ProxyTest.java
+++ security-proxy/src/test/java/org/georchestra/security/ProxyTest.java
@@ -29,7 +29,6 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.google.common.collect.Maps;
 
 public class ProxyTest {
@@ -67,6 +66,7 @@ public class ProxyTest {
         targets = Maps.newHashMap();
         targets.put("geonetwork", "http://www.google.com/geonetwork-private");
         targets.put("extractorapp", "http://localhost/extractorapp-private");
+        targets.put("nextcloud", "http://localhost/nextcloud");
         proxy.setTargets(targets);
 
         proxy.setDefaultTarget("/mapfishapp/");
@@ -261,16 +261,34 @@ public class ProxyTest {
     }
 
     @Test
-    public void testVerb() throws UnsupportedEncodingException {
+    public void testVerb() {
 
         for (RequestMethod e : RequestMethod.values()) {
             request = new MockHttpServletRequest(e.toString(), "/extractorapp/home");
             response = new BasicHttpResponse(HttpVersion.HTTP_1_1, HttpStatus.OK.value(), e.toString() + " worked");
             response.setHeader("X-Test-Header", e.toString() + " worked");
             httpResponse = new MockHttpServletResponse();
+
             proxy.handleRequest(request, httpResponse);
+
             assertEquals(HttpStatus.OK.value(), httpResponse.getStatus());
             assertEquals(e.toString() + " worked", httpResponse.getHeader("X-Test-Header"));
+        }
+    }
+
+    @Test
+    public void testWebDavVerb() {
+
+        for (String webDavVerb : Arrays.asList("PROPFIND", "SEARCH")) {
+            request = new MockHttpServletRequest(webDavVerb, "/nextcloud/plop");
+            response = new BasicHttpResponse(HttpVersion.HTTP_1_1, HttpStatus.OK.value(), webDavVerb + " worked");
+            response.setHeader("X-Test-Header", webDavVerb + " worked");
+            httpResponse = new MockHttpServletResponse();
+
+            proxy.handleRequest(request, httpResponse);
+
+            assertEquals(HttpStatus.OK.value(), httpResponse.getStatus());
+            assertEquals(webDavVerb + " worked", httpResponse.getHeader("X-Test-Header"));
         }
     }
 
