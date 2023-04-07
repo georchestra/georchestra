4f2f98afddd5f669be53edc02252ded8b59ff387
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2023-04-07 18:10:49 +0200

    ldap-acc-mgmt - updating fixtures / tests
    I can't get why it has not failed before, but since the removal of the
    EXTRACTORAPP role, this has to be updated as well.

--- ldap-account-management/src/test/java/org/georchestra/ds/security/InternalSecurityApiImplIT.java
+++ ldap-account-management/src/test/java/org/georchestra/ds/security/InternalSecurityApiImplIT.java
@@ -75,7 +75,7 @@ public class InternalSecurityApiImplIT {
         List<Role> defaultRoles = loadJson("/defaultRoles.json", Role.class);
         assertEquals(5, defaultUsers.size());
         assertEquals(2, defaultOrganizations.size());
-        assertEquals(11, defaultRoles.size());
+        assertEquals(10, defaultRoles.size());
 
         expectedUsers = toMap(defaultUsers, GeorchestraUser::getId);
         expectedOrganizations = toMap(defaultOrganizations, Organization::getId);
