b26946cd518546c55a4ac3370ce4e91730a11e21
Florian Necas <florian.necas@camptocamp.com>
2023-11-17 14:32:07 +0100

    feat: update quote to fix codacy static analysis

--- postgresql/docker-entrypoint-initdb.d/000-skip_data.sh
+++ postgresql/docker-entrypoint-initdb.d/000-skip_data.sh
@@ -4,5 +4,4 @@ for file in `ls /docker-entrypoint-initdb.d/*-data.sql`; do
   if [ "$IGNORE_DATA" = "yes" ]; then
    echo -n "" > $file
   fi
-  echo $file
 done
--- security-proxy/src/docker/docker-entrypoint.d/100-execute-custom-scripts.sh
+++ security-proxy/src/docker/docker-entrypoint.d/100-execute-custom-scripts.sh
@@ -6,6 +6,6 @@ if [[ -z "${CUSTOM_SCRIPTS_DIRECTORY}" ]]; then
 else
   echo "[INFO] CUSTOM_SCRIPTS_DIRECTORY env variable set to ${CUSTOM_SCRIPTS_DIRECTORY}"
   # Regex is needed in jetty9 images, but not alpine's ones.
-  run-parts -v ${CUSTOM_SCRIPTS_DIRECTORY} --regex='.*'
+  run-parts -v "${CUSTOM_SCRIPTS_DIRECTORY}" --regex='.*'
   echo "[INFO] End executing custom scripts"
 fi
