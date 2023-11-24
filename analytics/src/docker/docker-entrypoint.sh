8057957a10135a56502ede2cf668f1ffd920ffa2
Florian Necas <florian.necas@camptocamp.com>
2023-11-24 17:11:43 +0100

    feat: taking account of jeanpommier recommandation

--- analytics/src/docker/docker-entrypoint.sh
+++ analytics/src/docker/docker-entrypoint.sh
@@ -2,6 +2,16 @@
 
 DIR=/docker-entrypoint.d
 
+# Executing custom scripts located in CUSTOM_SCRIPTS_DIRECTORY if environment variable is set
+if [[ -z "${CUSTOM_SCRIPTS_DIRECTORY}" ]]; then
+  echo "[INFO] No CUSTOM_SCRIPTS_DIRECTORY env variable set"
+else
+  echo "[INFO] CUSTOM_SCRIPTS_DIRECTORY env variable set to ${CUSTOM_SCRIPTS_DIRECTORY}"
+  # Regex is needed in jetty9 images, but not alpine's ones.
+  mv -v "${CUSTOM_SCRIPTS_DIRECTORY}"/* "$DIR"
+  echo "[INFO] End moving custom scripts"
+fi
+
 if [[ -d "$DIR" ]]
 then
   # Regex is needed to execute all kind of files, including sh files. Warning : --regex not available in alpine images.
