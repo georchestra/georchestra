55cf689b8439660b9099e5a4ea1b870ff00d9a2c
Emilien Devos <emilien.devos@camptocamp.com>
2023-11-29 16:50:34 +0100

    standardize comments about regex in run-parts

--- ldap/docker-root/docker-entrypoint.sh
+++ ldap/docker-root/docker-entrypoint.sh
@@ -13,6 +13,7 @@ fi
 
 if [[ -d "$DIR" ]]
 then
+  # Regex is needed to execute all kind of files, including sh files. Warning : --regex not available in alpine images.
   /bin/run-parts --verbose "$DIR" --regex='.*'
 fi
 
--- postgresql/docker-entrypoint-initdb.d/200-execute-custom-scripts.sh
+++ postgresql/docker-entrypoint-initdb.d/200-execute-custom-scripts.sh
@@ -5,7 +5,7 @@ if [[ -z "${CUSTOM_SCRIPTS_DIRECTORY}" ]]; then
   echo "[INFO] No CUSTOM_SCRIPTS_DIRECTORY env variable set"
 else
   echo "[INFO] CUSTOM_SCRIPTS_DIRECTORY env variable set to ${CUSTOM_SCRIPTS_DIRECTORY}"
-  # Regex is needed in jetty9 images, but not alpine's ones.
+  # Regex is needed to execute all kind of files, including sh files. Warning : --regex not available in alpine images.
   run-parts -v "${CUSTOM_SCRIPTS_DIRECTORY}" --regex='.*'
   echo "[INFO] End executing custom scripts"
 fi
