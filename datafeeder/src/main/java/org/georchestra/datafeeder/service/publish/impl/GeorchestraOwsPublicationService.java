1abc3246c40326cd6d6cf1e41257bc1c1bb4a0cf
Florian Necas <florian.necas@camptocamp.com>
2024-09-26 16:12:54 +0200

    Merge pull request #4346 from georchestra/warning-if-schema-is-desync
    datafeeder - Don't override conf and warn if local and geoserver schema are desync

--- datafeeder/src/main/java/org/georchestra/datafeeder/service/publish/impl/GeorchestraOwsPublicationService.java
+++ datafeeder/src/main/java/org/georchestra/datafeeder/service/publish/impl/GeorchestraOwsPublicationService.java
@@ -102,6 +102,11 @@ public class GeorchestraOwsPublicationService implements OWSPublicationService {
         requireNonNull(publishing.getImportedName(),
                 "importedName is required to resolve the native feature type name");
 
+        final Map<String, String> geoserverConfig = this.configProperties.getPublishing().getBackend().getGeoserver();
+        final String localSchema = this.configProperties.getPublishing().getBackend().getLocal().get("schema");
+        if (!Objects.equals(localSchema, geoserverConfig.get("schema"))) {
+            log.warn("Local schema {} does not match geoserver schema {}", localSchema, geoserverConfig.get("schema"));
+        }
         final String workspaceName = resolveWorkspace(user);
         final String dataStoreName = nameResolver.resolveDataStoreName(workspaceName);
         final String publishedLayerName = resolveUniqueLayerName(workspaceName, publishing.getPublishedName());
