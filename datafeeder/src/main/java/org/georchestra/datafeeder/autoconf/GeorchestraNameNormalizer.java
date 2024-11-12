edc501ff2c0f2537fef876e0e4fc1fc8083b080a
Florian Necas <florian.necas@camptocamp.com>
2024-11-12 17:55:37 +0100

    fix: resolve dataabse table name in links

--- datafeeder/src/main/java/org/georchestra/datafeeder/autoconf/GeorchestraNameNormalizer.java
+++ datafeeder/src/main/java/org/georchestra/datafeeder/autoconf/GeorchestraNameNormalizer.java
@@ -38,8 +38,8 @@ public class GeorchestraNameNormalizer {
     }
 
     /**
-     * @return lower-cased {@link #normalizeName normalized} {@code title}
-     * 60 chars cause postgis table name cannot be more than 63 characters
+     * @return lower-cased {@link #normalizeName normalized} {@code title} 60 chars
+     *         cause postgis table name cannot be more than 63 characters
      */
     public @NonNull String resolveDatabaseTableName(@NonNull String title) {
         return normalizeName(title).toLowerCase().substring(0, Math.min(60, title.length()));
