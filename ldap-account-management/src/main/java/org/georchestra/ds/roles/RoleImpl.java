a3c81479183828e6a3a58a1ab53aad964cce7a52
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-03-11 15:00:35 +0100

    refactor

--- ldap-account-management/src/main/java/org/georchestra/ds/roles/RoleImpl.java
+++ ldap-account-management/src/main/java/org/georchestra/ds/roles/RoleImpl.java
@@ -19,13 +19,22 @@
 
 package org.georchestra.ds.roles;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.naming.InvalidNameException;
+import javax.naming.ldap.LdapName;
+import javax.naming.ldap.Rdn;
 
 /**
  * A role and its users.
@@ -78,8 +87,6 @@ class RoleImpl implements Role {
     }
 
     /*
-     * (non-Javadoc)
-     *
      * @see org.georchestra.console.dto.Role#setMemberUid(java.util.List)
      */
     @Override
@@ -95,8 +102,6 @@ class RoleImpl implements Role {
     }
 
     /*
-     * (non-Javadoc)
-     *
      * @see org.georchestra.console.dto.Role#addMemberUid(java.lang.String)
      */
     @Override
@@ -109,6 +114,39 @@ class RoleImpl implements Role {
     public void addOrg(String orgUid) {
         // Extracting the uid
         this.orgList.add(orgUid.replaceAll("cn=([^,]+).*$", "$1"));
+    }
+
+    @Override
+    public void addMembers(String[] members) {
+        Map<String, List<String>> membersByOu = Arrays.stream(Optional.ofNullable(members).orElse(new String[0])) //
+                .map(Members::new) //
+                .collect(
+                        Collectors.groupingBy(Members::getOu, Collectors.mapping(Members::getId, Collectors.toList())));
+        orgList.addAll(membersByOu.getOrDefault("orgs", List.of()));
+        userList.addAll(membersByOu.getOrDefault("users", List.of()));
+    }
+
+    @Getter
+    private static class Members {
+        private String id;
+        private String ou = "unknown";
+
+        public Members(String dn) {
+            try {
+                LdapName ldapName = new LdapName(dn);
+                List<String> organizationalUnits = ldapName.getRdns().stream() //
+                        .filter(rdn -> "ou".equals(rdn.getType())) //
+                        .map(Rdn::getValue).map(Objects::toString).collect(Collectors.toList());
+                if (organizationalUnits.contains("orgs") && !organizationalUnits.contains("users")) {
+                    ou = "orgs";
+                    id = dn.replaceAll("cn=([^,]+).*$", "$1");
+                } else if (!organizationalUnits.contains("orgs") && organizationalUnits.contains("users")) {
+                    ou = "users";
+                    id = dn.replaceAll("uid=([^,]+).*$", "$1");
+                }
+            } catch (InvalidNameException e) {
+            }
+        }
     }
 
     @Override
